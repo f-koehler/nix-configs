6cf30eee988d6bc0b9a0ad19d956c231c1e04a34
Fabian Koehler <me@fkoehler.org>
2023-08-20 00:54:52 +0800

    Add home-manager to nixos config.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,12 +1,22 @@
 {
   inputs = {
     hyprland.url = "github:hyprwm/Hyprland";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, ... } @ inputs: {
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs: {
     nixosConfigurations.fknix = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.fkoehler = import ../home-manager/home.nix;
+        }
+      ];
     };
   };
 }

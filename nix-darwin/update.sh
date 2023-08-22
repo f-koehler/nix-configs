773464dd9f13ffe9c2d30846e7df2d09feeeaca9
Fabian Köhler <fabian.koehler@protonmail.ch>
2023-08-22 22:28:04 +0200

    Extend update script.

--- nix-darwin/update.sh
+++ nix-darwin/update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -euf -o pipefail
-nix-channel --update darwin
+nix-channel --update
 darwin-rebuild switch --flake ${HOME}/Code/configs/nix-darwin/
+
+/opt/homebrew/bin/brew upgrade

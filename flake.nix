d5578ea737c4a441ffd80182e8522e00df14fadc
Fabian Köhler <fabian.koehler@protonmail.ch>
2023-08-23 09:16:16 +0200

    Start migrating to single flake file.

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs, ... }: {
+    darwinConfigurations."mbp2021" = nix-darwin.lib.darwinSystem {
+      modules = [
+        ./nix-darwin/configuration.nix
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.fkoehler = import ./home-manager/home.nix;
+        }
+      ];
+    };
+
+    homeConfigurations."fkoehler@fke15" = home-manager.lib.homeManagerConfiguration {
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+      modules = [
+        ./home.nix
+        {
+          home = {
+            username = "fkoehler";
+            homeDirectory = "/home/fkoehler";
+          };
+        }
+      ];
+    };
+  };
+}

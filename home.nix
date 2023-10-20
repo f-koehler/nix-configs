0030dd5d432eaf5ab3e45a59df08c3b7c80d65af
Fabian Köhler <me@fkoehler.org>
2023-10-21 02:47:57 +0800

    Manage nix settings in home-manager.

--- home.nix
+++ home.nix
@@ -1,6 +1,10 @@
 { config, pkgs, lib, ... }:
 
 {
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
   home = {
     # Home Manager needs a bit of information about you and the paths it should
     # manage.

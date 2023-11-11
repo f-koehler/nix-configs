1ff3242a61814b6c7f5b5dab27c8be651d0f4ff4
Fabian Köhler <me@fkoehler.org>
2023-11-11 21:22:53 +0800

    Fix home-manager workflow in GHA.

--- flake.nix
+++ flake.nix
@@ -69,6 +69,22 @@
           }
         ];
       };
+      "gha" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+        modules = [
+          nix-index-database.hmModules.nix-index
+          ./home/default.nix
+          {
+            home = {
+              username = "runner";
+              homeDirectory = "/home/runner";
+            };
+          }
+        ];
+      };
     };
   };
 }

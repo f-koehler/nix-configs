91862a7b0a844908801aea3ffda143f30d7e1439
Fabian Köhler <me@fkoehler.org>
2023-11-21 16:11:40 +0800

    Disable awscli.

--- home/default.nix
+++ home/default.nix
@@ -38,7 +38,7 @@
     # The home.packages option allows you to install Nix packages into your
     # environment.
     packages = with pkgs; [
-      awscli2
+      # awscli2
       bat
       du-dust
       fish

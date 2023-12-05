3033b69e26b01df7cae9513e53cc31ea9553d07f
Fabian Köhler <me@fkoehler.org>
2023-12-05 12:23:33 +0800

    Create tailscale alias on Mac OS.

--- home/default.nix
+++ home/default.nix
@@ -111,7 +111,7 @@
     "mv" = "mv -i";
     "rm" = "rm -i";
     "cat" = "bat --style=plain --paging=never";
-    # "tailscale" = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
+    "tailscale" = lib.mkIf pkgs.stdenv.isDarwin "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
     "open" = lib.mkIf pkgs.stdenv.isLinux "xdg-open";
   };
 

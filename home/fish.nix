5b64f16301c93d3c6f54b868af88dd736212856f
Fabian Köhler <me@fkoehler.org>
2023-12-07 14:17:48 +0800

    Add load_secret function and direnv hook to fish.

--- home/fish.nix
+++ home/fish.nix
@@ -3,6 +3,16 @@ _: {
     enable = true;
     shellInit = ''
       eval "$(micromamba shell hook --shell=fish)"
+      direnv hook fish | source
     '';
+    functions = {
+      "load_secrets" = ''
+        set -g -x AWS_ACCESS_KEY_ID "$(pass ls speqtral/aws/access_key_id)"
+        set -g -x AWS_SECRET_ACCESS_KEY "$(pass ls speqtral/aws/secret_access_key)"
+        set -g -x BW_CLIENTID "$(pass ls bitwarden/client_id)"
+        set -g -x BW_CLIENTSECRET "$(pass ls bitwarden/client_secret)"
+        set -g -x GH_TOKEN "$(pass ls github/tokens/cli)"
+      '';
+    };
   };
 }

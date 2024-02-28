558535a976bc8cd960cd0a817924a041e9ce37a9
Fabian Köhler <me@fkoehler.org>
2024-02-28 15:13:57 +0800

    Start nginx after dhparams have been generated and use them in the config.

--- nixos/modules/dhparams.nix
+++ nixos/modules/dhparams.nix
@@ -1,8 +1,10 @@
-_: {
+{config, ...}: {
   security.dhparams = {
     enable = true;
     stateful = true;
     defaultBitSize = 8192;
     path = "/etc/ssl/certs/dhparams.pem";
   };
+  systemd.services.nginx.after = ["dhparams-init.service"];
+  services.nginx.sslDhparam = "${config.security.dhparams.path}";
 }

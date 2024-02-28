d754460d49e7ac502d9de619226144d0a0a2a798
Fabian Köhler <me@fkoehler.org>
2024-02-28 15:33:54 +0800

    Configure reverse proxy for audiobookshelf.

--- nixos/modules/audiobookshelf.nix
+++ nixos/modules/audiobookshelf.nix
@@ -1,8 +1,36 @@
 _: {
-  services.audiobookshelf = {
-    enable = true;
-    openFirewall = true;
-    host = "0.0.0.0";
-    port = 8097;
+  services = {
+    audiobookshelf = {
+      enable = true;
+      openFirewall = true;
+      host = "0.0.0.0";
+      port = 8097;
+      user = "audiobookshelf";
+      group = "audiobookshelf";
+    };
+    nginx = {
+      upstreams."audiobookshelf" = {
+        servers = {
+          "127.0.0.1:8097" = {};
+        };
+      };
+      virtualHosts."https".locations = {
+        "/audiobookshelf" = {
+          return = "302 $scheme://$host/audiobookshelf/";
+        };
+        "/audiobookshelf/" = {
+          proxyPass = "http://audiobookshelf";
+          extraConfig = ''
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header Host $host;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "upgrade";
+
+            proxy_http_version 1.1;
+          '';
+        };
+      };
+    };
   };
 }

637a886b13121698cccf10139bb05dec78938137
Fabian Köhler <me@fkoehler.org>
2024-02-28 16:08:04 +0800

    Fix devShells in flake output schema.

--- flake.nix
+++ flake.nix
@@ -152,7 +152,7 @@
             };
           };
         };
-        devShell = inputs.nixpkgs.legacyPackages.${system}.mkShell {
+        devShells."${system}" = inputs.nixpkgs.legacyPackages.${system}.mkShell {
           inherit (self.checks.${system}.pre-commit-check) shellHook;
         };
       }

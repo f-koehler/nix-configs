81d2a8046f0dfc60433d4d6d6a65d5c27f0c9479
Fabian Köhler <me@fkoehler.org>
2024-02-28 15:35:20 +0800

    Use upstream for jellyfin reverse proxy config.

--- nixos/modules/jellyfin.nix
+++ nixos/modules/jellyfin.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     jellyfin
     jellyfin-web
@@ -15,28 +15,35 @@
       enable = true;
       openFirewall = true;
     };
-    nginx.virtualHosts."https".locations = {
-      "/jellyfin" = {
-        return = "302 $scheme://$host/jellyfin/";
+    nginx = {
+      upstreams."jellyfin" = {
+        servers = {
+          "127.0.0.1:8096" = {};
+        };
       };
-      "/jellyfin/" = {
-        recommendedProxySettings = true;
-        proxyPass = "http://127.0.0.1:8096";
-        extraConfig = ''
-          proxy_pass_request_headers on;
-          proxy_set_header Host $host;
+      virtualHosts."https".locations = {
+        "/jellyfin" = {
+          return = "302 $scheme://$host/jellyfin/";
+        };
+        "/jellyfin/" = {
+          recommendedProxySettings = true;
+          proxyPass = "http://jellyfin";
+          extraConfig = ''
+            proxy_pass_request_headers on;
+            proxy_set_header Host $host;
 
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-          proxy_set_header X-Forwarded-Host $http_host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header X-Forwarded-Host $http_host;
 
-          proxy_set_header Upgrade $http_upgrade;
-          proxy_set_header Connection $http_connection;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection $http_connection;
 
-          # Disable buffering when the nginx proxy gets very resource heavy upon streaming
-          proxy_buffering off;
-        '';
+            # Disable buffering when the nginx proxy gets very resource heavy upon streaming
+            proxy_buffering off;
+          '';
+        };
       };
     };
   };

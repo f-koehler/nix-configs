2cbdb95751c787fd3fa8371e4bfb945d22fb29b2
Fabian Köhler <me@fkoehler.org>
2024-02-28 15:35:48 +0800

    Use pre-commit-hooks.nix to run pre-commit hooks.

--- flake.nix
+++ flake.nix
@@ -46,77 +46,100 @@
       url = "github:outfoxxed/hy3";
       inputs.hyprland.follows = "hyprland";
     };
+
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs: {
-    darwinConfigurations."mac_arm64" = inputs.nix-darwin.lib.darwinSystem {
-      modules = [
-        {
-          nixpkgs.overlays = [
-            inputs.nix-vscode-extensions.overlays.default
-          ];
-        }
-        inputs.nix-index-database.darwinModules.nix-index
-        ./macos/default.nix
-        inputs.home-manager.darwinModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.fkoehler = import ./home/default.nix;
-          };
-        }
-      ];
-    };
-
-    homeConfigurations = {
-      "linux_x64" = inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = import inputs.nixpkgs {
-          system = "x86_64-linux";
-          config.allowUnfree = true;
-        };
+  outputs = {self, ...} @ inputs:
+    {
+      darwinConfigurations."mac_arm64" = inputs.nix-darwin.lib.darwinSystem {
         modules = [
           {
             nixpkgs.overlays = [
               inputs.nix-vscode-extensions.overlays.default
-              inputs.nixgl.overlay
             ];
           }
-          inputs.nix-index-database.hmModules.nix-index
-          inputs.hyprland.homeManagerModules.default
-          ./home/default.nix
+          inputs.nix-index-database.darwinModules.nix-index
+          ./macos/default.nix
+          inputs.home-manager.darwinModules.home-manager
           {
-            home = {
-              username = "fkoehler";
-              homeDirectory = "/home/fkoehler";
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.fkoehler = import ./home/default.nix;
             };
           }
-          {
-            wayland.windowManager.hyprland = {
-              enable = true;
-              plugins = [
-                inputs.hy3.packages.x86_64-linux.hy3
+        ];
+      };
+
+      homeConfigurations = {
+        "linux_x64" = inputs.home-manager.lib.homeManagerConfiguration {
+          pkgs = import inputs.nixpkgs {
+            system = "x86_64-linux";
+            config.allowUnfree = true;
+          };
+          modules = [
+            {
+              nixpkgs.overlays = [
+                inputs.nix-vscode-extensions.overlays.default
+                inputs.nixgl.overlay
               ];
-            };
-          }
-        ];
-      };
-      "gha" = inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = import inputs.nixpkgs {
-          system = "x86_64-linux";
-          config.allowUnfree = true;
+            }
+            inputs.nix-index-database.hmModules.nix-index
+            inputs.hyprland.homeManagerModules.default
+            ./home/default.nix
+            {
+              home = {
+                username = "fkoehler";
+                homeDirectory = "/home/fkoehler";
+              };
+            }
+            {
+              wayland.windowManager.hyprland = {
+                enable = true;
+                plugins = [
+                  inputs.hy3.packages.x86_64-linux.hy3
+                ];
+              };
+            }
+          ];
         };
-        modules = [
-          inputs.nix-index-database.hmModules.nix-index
-          ./home/default.nix
-          {
-            home = {
-              username = "runner";
-              homeDirectory = "/home/runner";
-            };
-          }
-        ];
+        "gha" = inputs.home-manager.lib.homeManagerConfiguration {
+          pkgs = import inputs.nixpkgs {
+            system = "x86_64-linux";
+            config.allowUnfree = true;
+          };
+          modules = [
+            inputs.nix-index-database.hmModules.nix-index
+            ./home/default.nix
+            {
+              home = {
+                username = "runner";
+                homeDirectory = "/home/runner";
+              };
+            }
+          ];
+        };
       };
-    };
-  };
+    }
+    // inputs.flake-utils.lib.eachDefaultSystem (
+      system: {
+        checks = {
+          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              alejandra.enable = true;
+              deadnix.enable = true;
+              nil.enable = true;
+              statix.enable = true;
+            };
+          };
+        };
+        devShell = inputs.nixpkgs.legacyPackages.${system}.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        };
+      }
+    );
 }

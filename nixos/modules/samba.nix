d19c371ad1ca6555e9296cf7aa5acb66d1cfc9d2
Fabian Köhler <me@fkoehler.org>
2024-02-28 15:36:17 +0800

    Reformat samba.nix.

--- nixos/modules/samba.nix
+++ nixos/modules/samba.nix
@@ -1,4 +1,8 @@
-{ pkgs, config, ... }: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   environment.systemPackages = with pkgs; [
     config.services.samba.package
     cifs-utils
@@ -23,7 +27,7 @@
       directory mask = 0775
       force directory mode = 0775
       follow symlinks = yes
-      
+
       guest account = nobody
       map to guest = bad user
       hosts allow = 100.64.0.0/10, 192.168.50., localhost, 127.0.0.1

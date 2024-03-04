720e172d44dcad49d806724d475566138717096f
Fabian Köhler <me@fkoehler.org>
2024-03-04 23:58:51 +0800

    Configure reverse proxy for audiobookshelf.

--- nixos/modules/audiobookshelf.nix
+++ nixos/modules/audiobookshelf.nix
@@ -14,21 +14,35 @@ _: {
           "127.0.0.1:8097" = {};
         };
       };
-      virtualHosts."https".locations = {
-        "/audiobookshelf" = {
-          return = "302 $scheme://$host/audiobookshelf/";
-        };
-        "/audiobookshelf/" = {
-          proxyPass = "http://audiobookshelf";
-          extraConfig = ''
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header Host $host;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "upgrade";
-
-            proxy_http_version 1.1;
-          '';
+      virtualHosts.audiobookshelf = {
+        forceSSL = true;
+        sslCertificate = "/etc/ssl/certs/tailscale.crt";
+        sslCertificateKey = "/etc/ssl/certs/tailscale.key";
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 48097;
+            ssl = true;
+          }
+          {
+            addr = "[::]";
+            port = 48097;
+            ssl = true;
+          }
+        ];
+        locations = {
+          "/" = {
+            proxyPass = "http://audiobookshelf/";
+            extraConfig = ''
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+              proxy_set_header Host $host;
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection "upgrade";
+              proxy_http_version 1.1;
+              proxy_redirect http:// https://;
+            '';
+          };
         };
       };
     };
--- nixos/modules/nginx.nix
+++ nixos/modules/nginx.nix
@@ -16,30 +16,6 @@
     recommendedGzipSettings = true;
     clientMaxBodySize = "10G";
     sslDhparam = "${config.security.dhparams.params.nginx.path}";
-    virtualHosts."https" = {
-      serverName = "${config.networking.hostName}.corgi-dojo.ts.net";
-      http2 = true;
-      kTLS = true;
-      forceSSL = true;
-      sslCertificate = "/etc/ssl/certs/tailscale.crt";
-      sslCertificateKey = "/etc/ssl/certs/tailscale.key";
-      listen = [
-        {
-          addr = "0.0.0.0";
-          port = 443;
-          ssl = true;
-        }
-        {
-          addr = "[::]";
-          port = 443;
-          ssl = true;
-        }
-      ];
-      extraConfig = ''
-        proxy_headers_hash_max_size 512;
-        proxy_headers_hash_bucket_size 128;
-      '';
-    };
   };
   networking.firewall.allowedTCPPorts = [80 443];
   systemd.services.nginx.after = [

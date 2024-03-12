d9d014b8ee4b76726dcc105d82bca80ccbd21861
Fabian Köhler <me@fkoehler.org>
2024-03-12 20:26:53 +0800

    Clean up vscode config.

--- home/vscode.nix
+++ home/vscode.nix
@@ -9,9 +9,12 @@
     enableUpdateCheck = pkgs.stdenv.isDarwin;
     extensions =
       [
+        pkgs.vscode-marketplace.akiramiyakoda.cppincludeguard
         pkgs.vscode-marketplace.asvetliakov.vscode-neovim
         pkgs.vscode-marketplace.charliermarsh.ruff
+        pkgs.vscode-marketplace.eamodio.gitlens
         pkgs.vscode-marketplace.editorconfig.editorconfig
+        pkgs.vscode-marketplace.efanzh.graphviz-preview
         pkgs.vscode-marketplace.esbenp.prettier-vscode
         pkgs.vscode-marketplace.github.copilot
         pkgs.vscode-marketplace.github.vscode-github-actions
@@ -29,61 +32,63 @@
         pkgs.vscode-marketplace.ms-vscode-remote.remote-containers
         pkgs.vscode-marketplace.ms-vscode-remote.remote-ssh
         pkgs.vscode-marketplace.ms-vscode.cpptools
+        pkgs.vscode-marketplace.ms-vscode.cpptools-extension-pack
         pkgs.vscode-marketplace.ms-vscode.hexeditor
         pkgs.vscode-marketplace.ms-vsliveshare.vsliveshare
         pkgs.vscode-marketplace.redhat.vscode-xml
         pkgs.vscode-marketplace.redhat.vscode-yaml
+        pkgs.vscode-marketplace.stephanvs.dot
         pkgs.vscode-marketplace.tailscale.vscode-tailscale
         pkgs.vscode-marketplace.tamasfe.even-better-toml
         pkgs.vscode-marketplace.usernamehw.errorlens
         pkgs.vscode-marketplace.vscode-icons-team.vscode-icons
-        pkgs.vscode-marketplace.efanzh.graphviz-preview
-        pkgs.vscode-marketplace.stephanvs.dot
-        pkgs.vscode-marketplace.kamadorueda.alejandra
-        pkgs.vscode-marketplace.eamodio.gitlens
       ]
       ++ [
         pkgs.vscode-extensions.ms-vscode.cmake-tools
         pkgs.vscode-extensions.ms-vscode.makefile-tools
       ];
     userSettings = {
+      "[nix]" = {
+        "editor.defaultFormatter" = "jnoortheen.nix-ide";
+      };
+      "[python]" = {
+        "editor.defaultFormatter" = "charliermarsh.ruff";
+      };
+
       "C/C++ Include Guard.Macro Type" = "Filepath";
       "C/C++ Include Guard.Path Skip" = 1;
       "C/C++ Include Guard.Remove Extension" = false;
       "C_Cpp.clang_format_fallbackStyle" = "LLVM";
       "C_Cpp.codeAnalysis.clangTidy.enabled" = true;
+      "C_Cpp.formatting" = "clangFormat";
+
       "cmake.options.statusBarVisibility" = "compact";
       "cmake.showOptionsMovedNotification" = false;
+      "cmake.configureOnOpen" = true;
       "editor.fontSize" = 12;
       "editor.formatOnSave" = true;
+      "editor.fontFamily" = "Hack Nerd Font";
       "editor.minimap.enabled" = false;
       "extensions.experimental.affinity" = {
         "asvetliakov.vscode-neovim" = 1;
       };
-      "extensions.experimental.useUtilityProcess" = true;
       "git.autofetch" = true;
       "latex-workshop.latex.recipe.default" = "latexmk (lualatex)";
       "latex-workshop.hover.preview.mathjax.extensions" = ["braket"];
-      "redhat.telemetry.enabled" = false;
-      "terminal.integrated.fontFamily" = "Hack Nerd Font";
-      "terminal.integrated.fontSize" = 10;
-      "vsicons.dontShowNewVersionMessage" = true;
-      "workbench.iconTheme" = "vscode-icons";
-      "[python]" = {
-        "editor.defaultFormatter" = "charliermarsh.ruff";
-      };
-      "[nix]" = {
-        "editor.defaultFormatter" = "jnoortheen.nix-ide";
-      };
-      nix = {
-        enableLanguageServer = true;
-        serverPath = "${pkgs.nil}/bin/nil";
-        serverSettings = {
-          formatting = {
-            command = ["${pkgs.alejandra}/bin/alejandra"];
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "${pkgs.nil}/bin/nil";
+      "nix.serverSettings" = {
+        "nil" = {
+          "formatting" = {
+            "command" = ["${pkgs.alejandra}/bin/alejandra"];
           };
         };
       };
+      "redhat.telemetry.enabled" = false;
+      "terminal.integrated.fontFamily" = "Hack Nerd Font";
+      "terminal.integrated.fontSize" = 12;
+      "vsicons.dontShowNewVersionMessage" = true;
+      "workbench.iconTheme" = "vscode-icons";
     };
   };
 }

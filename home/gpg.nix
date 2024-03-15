290e037efdc8b3c500e592843352ed7f38c6d4aa
Fabian Köhler <me@fkoehler.org>
2024-03-15 10:56:34 +0800

    Update gpg-agent config w.r.t. home-manager changes.

--- home/gpg.nix
+++ home/gpg.nix
@@ -1,15 +1,15 @@
 {pkgs, ...}: {
-  services.gpg-agent =
-    if pkgs.stdenv.isDarwin
-    then {}
-    else {
-      enable = true;
-      enableBashIntegration = true;
-      enableFishIntegration = true;
-      enableZshIntegration = true;
-      defaultCacheTtl = 18000;
-      grabKeyboardAndMouse = true;
-      maxCacheTtl = 18000;
-      pinentryFlavor = "qt";
-    };
+  services.gpg-agent = {
+    enable = true;
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    enableZshIntegration = true;
+    defaultCacheTtl = 18000;
+    grabKeyboardAndMouse = true;
+    maxCacheTtl = 18000;
+    pinentryPackage =
+      if pkgs.stdenv.isDarwin
+      then pkgs.pinentry_mac
+      else pkgs.pinentry-qt;
+  };
 }

72f0c6724f269fb7a96f82cec20f66b30b088853
Fabian Köhler <me@fkoehler.org>
2024-03-19 01:37:20 +0800

    Setup docker on fkt14.

--- nixos/fkt14.nix
+++ nixos/fkt14.nix
@@ -40,8 +40,7 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  virtualisation.docker.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.fkoehler = {
@@ -51,6 +50,7 @@
       "libvirtd"
       "networkmanager"
       "wheel"
+      "docker"
     ];
     shell = pkgs.fish;
     packages = with pkgs; [

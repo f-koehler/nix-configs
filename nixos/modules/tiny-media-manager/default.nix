8b590b485f773f5b87896b4eb2cf837d2df55297
Fabian Köhler <me@fkoehler.org>
2024-03-19 01:36:14 +0800

    Refactor tmm nix file.

--- nixos/modules/tiny-media-manager/default.nix
+++ nixos/modules/tiny-media-manager/default.nix
@@ -1,8 +1,4 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   users = {
     groups.tmm = {};
     users.tmm = rec {
@@ -56,40 +52,38 @@
     # };
   };
   services = {
-    nginx = {
-      upstreams."tmm" = {
-        servers = {
-          "127.0.0.1:4000" = {};
-        };
+    nginx.upstreams."tmm" = {
+      servers = {
+        "127.0.0.1:4000" = {};
       };
-      virtualHosts."tmm.fkoehler.xyz" = {
-        forceSSL = true;
-        kTLS = true;
-        sslCertificate = "/var/lib/acme/fkoehler.xyz/fullchain.pem";
-        sslCertificateKey = "/var/lib/acme/fkoehler.xyz/key.pem";
-        listen = [
-          {
-            addr = "0.0.0.0";
-            port = 443;
-            ssl = true;
-          }
-        ];
-        locations = {
-          "/" = {
-            proxyPass = "http://tmm/";
-            extraConfig = ''
-              proxy_http_version 1.1;
-              proxy_set_header Upgrade $http_upgrade;
-              proxy_set_header Connection "upgrade";
+    };
+    nginx.virtualHosts."tmm.fkoehler.xyz" = {
+      forceSSL = true;
+      kTLS = true;
+      sslCertificate = "/var/lib/acme/fkoehler.xyz/fullchain.pem";
+      sslCertificateKey = "/var/lib/acme/fkoehler.xyz/key.pem";
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 443;
+          ssl = true;
+        }
+      ];
+      locations = {
+        "/" = {
+          proxyPass = "http://tmm/";
+          extraConfig = ''
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "upgrade";
 
-              proxy_redirect off;
-              proxy_set_header Host $host;
-              proxy_set_header X-Real-IP $remote_addr;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_set_header X-Forwarded-Host $server_name;
-              add_header Referrer-Policy "strict-origin-when-cross-origin";
-            '';
-          };
+            proxy_redirect off;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Host $server_name;
+            add_header Referrer-Policy "strict-origin-when-cross-origin";
+          '';
         };
       };
     };

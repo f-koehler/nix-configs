b5eab860fb88ec5c23edd5c98d3d1f431b8a3b96
Fabian Köhler <me@fkoehler.org>
2024-03-19 23:43:41 +0800

    Configure atuin sync.

--- home/atuin.nix
+++ home/atuin.nix
@@ -1,13 +1,18 @@
-_: {
+{config, ...}: {
   programs.atuin = {
     enable = true;
     enableBashIntegration = true;
     enableFishIntegration = true;
     enableZshIntegration = true;
     settings = {
-      style = "compact";
+      auto_sync = true;
       inline_height = 20;
+      key_path = config.sops.secrets."atuin/key".path;
       show_preview = true;
+      style = "compact";
+      sync_address = "https://api.atuin.sh";
+      sync_frequency = "5m";
+      update_check = false;
     };
     flags = [
       "--disable-up-arrow"

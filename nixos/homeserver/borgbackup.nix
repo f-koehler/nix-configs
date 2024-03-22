fdb55d5b97f3f6a85a2b711b79f7ad300f93b2e6
Fabian Köhler <me@fkoehler.org>
2024-03-22 11:44:22 +0800

    Add SSH key for jellyfin borg backup.

--- nixos/homeserver/borgbackup.nix
+++ nixos/homeserver/borgbackup.nix
@@ -1,4 +1,10 @@
 {config, ...}: {
+  sops.secrets = {
+    "services/jellyfin/ssh_key" = {
+      owner = "${config.services.jellyfin.user}";
+      group = "${config.services.jellyfin.group}";
+    };
+  };
   services.borgbackup = {
     repos = {
       jellyfin = {
@@ -6,17 +12,19 @@
         group = "${config.services.jellyfin.group}";
         quota = "50G";
         path = "/media/tank0/backups/homeserver/jellyfin";
+        authorizedKeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRgxSe/5JyfZYKV6uF0jw65zpxYcosrua2V6qHa2h3b jellyfin@homeserver";
       };
     };
     jobs = {
       jellyfin = {
         paths = ["/var/lib/jellyfin"];
-        repo = "jellyfin";
+        repo = "${config.services.jellyfin.user}@localhost:/${config.services.borgbackup.repos.jellyfin.path}";
         startAt = "daily";
         persistentTime = true;
         user = "${config.services.jellyfin.user}";
         group = "${config.services.jellyfin.group}";
         inhibitsSleep = true;
+        environment.BORG_RSH = "ssh -i ${config.sops.services.jellyfin.ssh_key.path}";
         prune.keep = {
           within = "14d";
           daily = 1;

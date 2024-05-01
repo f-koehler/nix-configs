2ff7fa24a2649731232f73b143656f0d084dd6a3
Fabian Köhler <me@fkoehler.org>
2024-05-02 00:13:21 +0800

    Tweak plasma config.

--- home/modules/plasma.nix
+++ home/modules/plasma.nix
@@ -7,12 +7,19 @@
   programs.plasma = lib.mkIf (pkgs.stdenv.isLinux && isWorkstation) {
     enable = true;
     workspace = {
+      clickItemTo = "select";
+      lookAndFeel = "org.kde.breezedark.desktop";
       wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Mountain/contents/images_dark/5120x2880.png";
     };
     hotkeys.commands."launch-wezterm" = {
       name = "Launch wezterm";
       key = "Meta+Return";
       command = "wezterm";
+    };
+    hotkeys.commands."launch-krunner" = {
+      name = "Launch krunner";
+      key = "Meta+D";
+      command = "krunner";
     };
     configFile = {
       "baloofilerc"."Basic Settings"."Indexing-Enabled".value = false;

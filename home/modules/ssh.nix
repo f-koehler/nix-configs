84a3e46a1e62ea1daf2cc5f610e2c5eefd61e712
Fabian Köhler <me@fkoehler.org>
2024-06-06 22:51:55 +0800

    Cleanup SSH config.

--- home/modules/ssh.nix
+++ home/modules/ssh.nix
@@ -1,4 +1,9 @@
-{config, ...}: {
+{
+  isWorkstation,
+  lib,
+  ...
+}:
+lib.mkIf isWorkstation {
   programs.ssh = {
     enable = true;
     matchBlocks = {
@@ -11,21 +16,6 @@
         hostname = "100.74.108.18";
         port = 20257;
         user = "fkoehler";
-      };
-      "osmc" = {
-        hostname = "100.82.94.54";
-        port = 22;
-        user = "osmc";
-      };
-      "fk-dev-x86-01" = {
-        hostname = "ec2-18-140-197-34.ap-southeast-1.compute.amazonaws.com";
-        user = "ubuntu";
-        identityFile = "${config.home.homeDirectory}/.ssh/fk.pem";
-      };
-      "fk-dev-x86-02" = {
-        hostname = "ec2-13-214-148-20.ap-southeast-1.compute.amazonaws.com";
-        user = "ubuntu";
-        identityFile = "${config.home.homeDirectory}/.ssh/fk.pem";
       };
       "homeserver" = {
         hostname = "100.88.7.127";

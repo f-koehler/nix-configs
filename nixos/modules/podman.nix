22a7a761778cbc528b7ea5a6d4e63ba382a1d48a
Fabian Köhler <me@fkoehler.org>
2024-06-08 22:52:09 +0800

    Enable autoPrune for podman and remove podman-compose service.

--- nixos/modules/podman.nix
+++ nixos/modules/podman.nix
@@ -8,21 +8,6 @@
   ];
   virtualisation.podman = {
     enable = true;
-  };
-  systemd.user.services."podman-compose@" = {
-    description = "Description=%i rootless pod (podman-compose)";
-    wantedBy = ["default.target"];
-    path = with pkgs; [
-      podman
-      podman-compose
-      shadow # for newuidmap
-      su # for newuidmap
-    ];
-    serviceConfig = {
-      Type = "simple";
-      EnvironmentFile = "%h/.config/containers/compose/projects/%i.env";
-      ExecStart = "${pkgs.podman-compose}/bin/podman-compose up";
-      ExecStop = "${pkgs.podman-compose}/bin/podman-compose down";
-    };
+    autoPrune.enable = true;
   };
 }

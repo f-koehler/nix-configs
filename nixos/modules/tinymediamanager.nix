944c7a4d204e4894a9b91a1a5033e4c9dbebb35f
Fabian Köhler <me@fkoehler.org>
2024-06-16 23:56:21 +0800

    Move tinymediamanager behind reverse proxy.

--- nixos/modules/tinymediamanager.nix
+++ nixos/modules/tinymediamanager.nix
@@ -31,4 +31,38 @@
       ];
     };
   };
+  services.nginx = {
+    upstreams.tinymediamanager = {
+      servers = {
+        "127.0.0.1:4000" = {};
+      };
+    };
+    virtualHosts."tinymediamanager.fkoehler.xyz" = {
+      forceSSL = true;
+      kTLS = true;
+      sslCertificate = "/var/lib/acme/fkoehler.xyz/fullchain.pem";
+      sslCertificateKey = "/var/lib/acme/fkoehler.xyz/key.pem";
+      http2 = true;
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 443;
+          ssl = true;
+        }
+      ];
+      locations = {
+        "/" = {
+          proxyPass = "http://tinymediamanager/";
+          extraConfig = ''
+            proxy_set_header   X-Real-IP $remote_addr;
+            proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header   Host $host;
+            proxy_http_version 1.1;
+            proxy_set_header   Upgrade $http_upgrade;
+            proxy_set_header   Connection "upgrade";
+          '';
+        };
+      };
+    };
+  };
 }

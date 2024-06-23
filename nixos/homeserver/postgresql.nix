0822583892f505c88a4e88ef2be0357f7fae03e8
Fabian Köhler <me@fkoehler.org>
2024-06-23 17:58:15 +0800

    Configure postgresql authentication.

---
+++ nixos/homeserver/postgresql.nix
@@ -0,0 +1,13 @@
+{pkgs, ...}: {
+  services.postgresql = {
+    enable = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method
+      local all       all     trust
+    '';
+    identMap = ''
+      superuser_map      root      postgres
+      superuser_map      postgres  postgres
+    '';
+  };
+}

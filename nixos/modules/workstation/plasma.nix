6096ac3537411a14fcd259488e98ba2f786f2e1f
Fabian Köhler <me@fkoehler.org>
2024-06-29 23:20:44 +0800

    Install some catppuccin related stuff for plasma.

--- nixos/modules/workstation/plasma.nix
+++ nixos/modules/workstation/plasma.nix
@@ -15,12 +15,22 @@
     };
     desktopManager.plasma6.enable = true;
   };
-  environment.systemPackages = with pkgs; [
-    kdePackages.skanpage
-    # kdePackages.zanshin
-    # kdePackages.merkuro
-    kdePackages.kcharselect
-  ];
+  environment.systemPackages = let
+    catppuccin-kde = pkgs.catppuccin-kde.override {flavour = ["mocha"];};
+  in
+    with pkgs;
+      [
+        kdePackages.skanpage
+        # kdePackages.zanshin
+        # kdePackages.merkuro
+        kdePackages.kcharselect
+        catppuccin-cursors
+        catppuccin-sddm
+        kdePackages.sddm-kcm
+        catppuccin-kde
+        catppuccin-cursors
+      ]
+      ++ [catppuccin-kde];
   security.pam.services.fkoehler.enableGnomeKeyring = true;
   services.gnome.gnome-keyring.enable = true;
 }

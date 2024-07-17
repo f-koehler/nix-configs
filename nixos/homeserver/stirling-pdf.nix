768cfccd3a8934e81dd4a0ed4a3d414508a886d1
Fabian Köhler <me@fkoehler.org>
2024-07-17 14:02:37 +0800

    Backup WIP jellystat and stirling PDF configs.

--- nixos/homeserver/stirling-pdf.nix
+++ nixos/homeserver/stirling-pdf.nix
@@ -4,4 +4,25 @@
       stirling-pdf
     ];
   };
+  users = {
+    groups.stirlingpdf = {};
+    users.stirlingpdf = {
+      isSystemUser = true;
+      group = "stirlingpdf";
+      home = "/var/lib/stirlingpdf";
+      createHome = true;
+    };
+  };
+  systemd.services."stirling-pdf" = {
+    description = "Stirling PDF";
+    wantedBy = ["multi-user.target"];
+    after = ["network.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.stirling-pdf}/bin/Stirling-PDF";
+      Restart = "on-failure";
+      User = "stirlingpdf";
+      Group = "stirlingpdf";
+    };
+  };
 }

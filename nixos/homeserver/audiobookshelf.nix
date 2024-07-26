805b4db64a95ab0b1b62d32c8b51b3a442f8d389
Fabian Köhler <me@fkoehler.org>
2024-07-26 14:08:17 +0800

    Try running audiobookshelf in a declarative container.

--- nixos/homeserver/audiobookshelf.nix
+++ nixos/homeserver/audiobookshelf.nix
@@ -1,45 +1,55 @@
 {config, ...}: {
-  services = {
-    audiobookshelf = {
-      enable = true;
-      host = "0.0.0.0";
-      port = 8097;
-      user = "audiobookshelf";
-      group = "audiobookshelf";
-    };
-    nginx = {
-      upstreams."audiobookshelf" = {
-        servers = {
-          "127.0.0.1:${toString config.services.audiobookshelf.port}" = {};
-        };
+  containers.audiobookshelf = {
+    bindMounts = {
+      "/var/lib/audiobookshelf" = {
+        hostPath = "/var/lib/audiobookshelf";
+        isReadOnly = false;
       };
-      virtualHosts."audiobooks.fkoehler.xyz" = {
-        forceSSL = true;
-        kTLS = true;
-        sslCertificate = "/var/lib/acme/fkoehler.xyz/fullchain.pem";
-        sslCertificateKey = "/var/lib/acme/fkoehler.xyz/key.pem";
-        listen = [
-          {
-            addr = "0.0.0.0";
-            port = 443;
-            ssl = true;
-          }
-        ];
-        locations = {
-          "/" = {
-            proxyPass = "http://audiobookshelf/";
-            extraConfig = ''
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_set_header X-Forwarded-Proto $scheme;
-              proxy_set_header Host $host;
-              proxy_set_header Upgrade $http_upgrade;
-              proxy_set_header Connection "upgrade";
-              proxy_http_version 1.1;
-              proxy_redirect http:// https://;
-            '';
-          };
-        };
+    };
+    config = _: {
+      services.audiobookshelf = {
+        enable = true;
+        host = "0.0.0.0";
+        port = 8097;
+        user = "audiobookshelf";
+        group = "audiobookshelf";
       };
     };
   };
+  # services = {
+  #   nginx = {
+  #     upstreams."audiobookshelf" = {
+  #       servers = {
+  #         "127.0.0.1:${toString config.services.audiobookshelf.port}" = {};
+  #       };
+  #     };
+  #     virtualHosts."audiobooks.fkoehler.xyz" = {
+  #       forceSSL = true;
+  #       kTLS = true;
+  #       sslCertificate = "/var/lib/acme/fkoehler.xyz/fullchain.pem";
+  #       sslCertificateKey = "/var/lib/acme/fkoehler.xyz/key.pem";
+  #       listen = [
+  #         {
+  #           addr = "0.0.0.0";
+  #           port = 443;
+  #           ssl = true;
+  #         }
+  #       ];
+  #       locations = {
+  #         "/" = {
+  #           proxyPass = "http://audiobookshelf/";
+  #           extraConfig = ''
+  #             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+  #             proxy_set_header X-Forwarded-Proto $scheme;
+  #             proxy_set_header Host $host;
+  #             proxy_set_header Upgrade $http_upgrade;
+  #             proxy_set_header Connection "upgrade";
+  #             proxy_http_version 1.1;
+  #             proxy_redirect http:// https://;
+  #           '';
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
 }

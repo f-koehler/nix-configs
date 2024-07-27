65783ed06d0b024569093808750e6cf3276f27ce
Fabian Köhler <fabian.koehler@protonmail.ch>
2024-07-28 00:31:15 +0800

    Add devenv to home config.

--- home/default.nix
+++ home/default.nix
@@ -118,6 +118,7 @@ in {
       ncdu
       htop
       cascadia-code
+      devenv
 
       # It is sometimes useful to fine-tune packages, for example, by applying
       # overrides. You can do that directly here, just don't forget the

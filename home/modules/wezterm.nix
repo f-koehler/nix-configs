90f453846d95b8ee38700295b7dffb31e7eb3c94
Fabian Köhler <me@fkoehler.org>
2024-08-26 04:06:09 +0800

    Simplify wezterm config.

--- home/modules/wezterm.nix
+++ home/modules/wezterm.nix
@@ -8,14 +8,10 @@
     enableBashIntegration = true;
     enableZshIntegration = true;
     extraConfig = ''
-      local wezterm = require 'wezterm';
-      local config = {}
-      if wezterm.config_builder then
-        config = wezterm.config_builder()
-      end
+      local config = wezterm.config_builder()
 
       config.audible_bell = "Disabled"
-      config.font = wezterm.font('Cascadia Code NF')
+      config.font = wezterm.font 'Cascadia Code NF'
       config.font_size = 9.0
       config.hide_tab_bar_if_only_one_tab = true
       config.color_scheme = "Catppuccin Mocha"

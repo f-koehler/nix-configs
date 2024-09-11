96447887ba732f25553136976ed4f9591d83e6cd
Fabian Köhler <me@fkoehler.org>
2024-09-12 01:08:16 +0800

    Update tinymediamanager 5.0.6 -> 5.0.11.

--- nixos/homeserver/tinymediamanager.nix
+++ nixos/homeserver/tinymediamanager.nix
@@ -12,7 +12,7 @@
   };
   virtualisation.oci-containers.containers = {
     tinymediamanager = {
-      image = "docker.io/tinymediamanager/tinymediamanager:5.0.6";
+      image = "docker.io/tinymediamanager/tinymediamanager:5.0.11";
       ports = [
         "127.0.0.1:5900:5900" # VNC port
         "127.0.0.1:4000:4000" # Webinterface

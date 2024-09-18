b341e4f896b7c5f15a17c5fee7f5805bde869dad
Fabian Köhler <me@fkoehler.org>
2024-09-18 13:28:29 +0800

    Move digikam db to host.

--- nixos/homeserver/wip/digikam-db.nix
+++ nixos/homeserver/wip/digikam-db.nix
@@ -1,12 +1,14 @@
-{config, ...}: let
-  port = 33306;
-in {
+{
+  config,
+  pkgs,
+  ...
+}: {
   sops = {
     secrets = {
       "services/digikam_db/password" = {};
     };
     templates = {
-      # Digikam DB initialization script from documentation: https://docs.digikam.org/en/setup_application/database_settings.html#the-mysql-remote-server
+      # Digikam DB initialization modified from Digikam documentation: https://docs.digikam.org/en/setup_application/database_settings.html#the-mysql-remote-server
       "digikam_db_initial_script.sql".content = ''
         CREATE USER 'digikam'@'%' IDENTIFIED BY '${config.sops.placeholder."services/digikam_db/password"}';
         GRANT ALL ON *.* TO 'digikam'@'%' IDENTIFIED BY '${config.sops.placeholder."services/digikam_db/password"}';
@@ -16,51 +18,22 @@ in {
       '';
     };
   };
-  networking.firewall.allowedTCPPorts = [port];
-  containers.digikam-db = {
-    ephemeral = true;
-    autoStart = true;
-    forwardPorts = [
-      {
-        hostPort = port;
-        containerPort = 3306;
-      }
-    ];
-    bindMounts = {
-      "/var/lib/mysql" = {
-        hostPath = "/containers/digikam-db";
-        isReadOnly = false;
-      };
-      "/share/digikam_db/initial_script.sql" = {
-        hostPath = config.sops.templates."digikam_db_initial_script.sql".path;
-        isReadOnly = true;
-      };
-    };
-    config = {
-      pkgs,
-      lib,
-      ...
-    }: {
-      services = {
-        mysql = {
-          enable = true;
-          package = pkgs.mariadb;
-          settings = {
-            mysqld = {
-              max_allowed_packet = "128M";
-              bind_address = "0.0.0.0";
-            };
-          };
-          initialScript = "/share/digikam_db/initial_script.sql";
+  services = {
+    mysql = {
+      enable = true;
+      package = pkgs.mariadb;
+      settings = {
+        mysqld = {
+          max_allowed_packet = "128M";
+          bind_address = "0.0.0.0";
         };
-        resolved.enable = true;
       };
-      networking.useHostResolvConf = lib.mkForce false;
+      initialScript = config.sops.templates."digikam_db_initial_script.sql".path;
     };
   };
   fileSystems = {
-    "/containers/digikam-db" = {
-      device = "rpool/digikam-db";
+    "/var/lib/mariadb" = {
+      device = "rpool/mariadb";
       fsType = "zfs";
     };
   };

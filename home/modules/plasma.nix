2c3d6a16c54fa640e24c8a63a4ca210a035bb1ef
Fabian Köhler <me@fkoehler.org>
2024-10-02 00:04:49 +0800

    Configure some plasma shortcuts.

--- home/modules/plasma.nix
+++ home/modules/plasma.nix
@@ -11,6 +11,33 @@
       clickItemTo = "select";
       wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Mountain/contents/images_dark/5120x2880.png";
     };
+    kwin.virtualDesktops.number = 10;
+    shortcuts = {
+      kwin = {
+        "Switch to Desktop 1" = "Meta+1";
+        "Switch to Desktop 2" = "Meta+2";
+        "Switch to Desktop 3" = "Meta+3";
+        "Switch to Desktop 4" = "Meta+4";
+        "Switch to Desktop 5" = "Meta+5";
+        "Switch to Desktop 6" = "Meta+6";
+        "Switch to Desktop 7" = "Meta+7";
+        "Switch to Desktop 8" = "Meta+8";
+        "Switch to Desktop 9" = "Meta+9";
+        "Switch to Desktop 10" = "Meta+0";
+        "Window to Desktop 1" = "Meta+Shift+1";
+        "Window to Desktop 2" = "Meta+Shift+2";
+        "Window to Desktop 3" = "Meta+Shift+3";
+        "Window to Desktop 4" = "Meta+Shift+4";
+        "Window to Desktop 5" = "Meta+Shift+5";
+        "Window to Desktop 6" = "Meta+Shift+6";
+        "Window to Desktop 7" = "Meta+Shift+7";
+        "Window to Desktop 8" = "Meta+Shift+8";
+        "Window to Desktop 9" = "Meta+Shift+9";
+        "Window to Desktop 10" = "Meta+Shift+0";
+        "Show Desktop" = "none";
+      };
+    };
+    spectacle.shortcuts.launch = "Print";
     hotkeys.commands."launch-wezterm" = {
       name = "Launch wezterm";
       key = "Meta+Return";

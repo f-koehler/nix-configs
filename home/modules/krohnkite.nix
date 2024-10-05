727fe2a308ea3b3a55c64b1612fb69aa7e3541de
Fabian Köhler <me@fkoehler.org>
2024-10-05 19:38:59 +0800

    Add module for some WIP tweaks for Krohnkite on plasma 6.

---
+++ home/modules/krohnkite.nix
@@ -0,0 +1,24 @@
+_: {
+  programs.plasma = {
+    configFile = {
+      kwinrc = {
+        Windows = {
+          ActiveMouseScreen = false;
+          SeparateScreenFocus = true;
+        };
+      };
+    };
+    window-rules = [
+      {
+        description = "Krohnkite: Set Minimum Geometry Size";
+        match = {
+          window-types = ["normal"];
+        };
+        apply."minsize" = {
+          apply = "force";
+          value = 2;
+        };
+      }
+    ];
+  };
+}

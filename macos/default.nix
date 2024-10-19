0c6edbc6e71b6388c08b9497c8f44eeaf7f5749d
Fabian Köhler <me@fkoehler.org>
2024-10-19 15:04:53 +0800

    Temporarily disable digikam install via homebrew as I want to test beta-build of 8.5.0.

--- macos/default.nix
+++ macos/default.nix
@@ -112,7 +112,7 @@
     onActivation.cleanup = "zap";
     casks = [
       "chatgpt"
-      "digikam"
+      # "digikam" currently using 8.5.0 prebuild for native M1 support and improved ML features
       "docker"
       "nextcloud"
       "obsidian"

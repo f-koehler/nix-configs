eddf4103215f82c26738028db8733626e99c0a0c
Fabian Köhler <me@fkoehler.org>
2024-12-03 10:56:23 +0800

    Tweak some zed settings.

--- home/modules/zed.nix
+++ home/modules/zed.nix
@@ -3,6 +3,10 @@ _: {
     enable = true;
     extensions = ["nix"];
     userSettings = {
+      buffer_font_family = "Cascadia Code NF";
+      buffer_font_size = 12;
+      telemetry.metrics = false;
+      ui_font_size = 14;
       vim_mode = true;
     };
   };

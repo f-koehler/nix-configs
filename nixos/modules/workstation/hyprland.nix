41bf170df1d5bd9bda22ac0103c4165419417c6a
Fabian Köhler <me@fkoehler.org>
2024-12-07 05:00:52 +0800

    Configure regreet and add some XDG config.

--- nixos/modules/workstation/hyprland.nix
+++ nixos/modules/workstation/hyprland.nix
@@ -1,8 +1,97 @@
-{pkgs, ...}: {
-  programs.hyprland = {
-    enable = true;
-    package = pkgs.hyprland;
-    portalPackage = pkgs.xdg-desktop-portal-hyprland;
-    systemd.setPath.enable = true;
+{
+  lib,
+  pkgs,
+  config,
+  username,
+  ...
+}: let
+  hyprlandLaunch = pkgs.writeShellScriptBin "hyprland-launch" ''
+    ${pkgs.hyprland}/bin/Hyprland $@ &>/dev/null
+
+    # Correctly clean up the session
+    ${pkgs.hyprland}/bin/hyprctl dispatch exit
+    systemctl --user --machine=${username}@.host stop dbus-broker
+    systemctl --user --machine=${username}@.host stop hyprland-session.target
+  '';
+in {
+  environment = {
+    systemPackages = [hyprlandLaunch];
   };
+  programs = {
+    hyprland = {
+      enable = true;
+      package = pkgs.hyprland;
+      portalPackage = pkgs.xdg-desktop-portal-hyprland;
+      systemd.setPath.enable = true;
+    };
+    nm-applet = lib.mkIf config.networking.networkmanager.enable {
+      enable = true;
+      indicator = true;
+    };
+    regreet = {
+      enable = true;
+      settings = {
+        appearance = {
+          greeting_msg = "Hello!";
+        };
+        commands = {
+          reboot = ["${pkgs.systemd}/bin/systemctl" "reboot"];
+          poweroff = ["${pkgs.systemd}/bin/systemctl" "poweroff"];
+        };
+        # GTK = lib.mkForce {
+        #   application_prefer_dark_theme = true;
+        #   cursor_theme_name = "catppuccin-mocha-blue-cursors";
+        #   font_name = "Work Sans 16";
+        #   icon_theme_name = "Papirus-Dark";
+        #   theme_name = "catppuccin-mocha-blue-standard";
+        # };
+      };
+    };
+  };
+  services = {
+    gvfs.enable = true;
+    udisks2.enable = true;
+    devmon.enable = true;
+    greetd = {
+      enable = true;
+      vt = 1;
+    };
+  };
+  security = {
+    pam.services = {
+      hyprlock = {};
+      greetd.enableGnomeKeyring = true;
+    };
+    polkit.enable = true;
+  };
+  xdg = {
+    portal = {
+      configPackages = [pkgs.hyprland];
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-hyprland
+        xdg-desktop-portal-gtk
+      ];
+      enable = true;
+      xdgOpenUsePortal = true;
+      config = {
+        common = {
+          default = ["gtk"];
+        };
+        hyprland = {
+          default = ["hyprland" "gtk"];
+          "org.freedesktop.impl.portal.Secret" = ["gnome-keyring"];
+        };
+      };
+    };
+    terminal-exec = {
+      enable = true;
+      settings = {
+        default = ["wezterm.desktop"];
+      };
+    };
+  };
+  # Fix xdg-portals opening URLs: https://github.com/NixOS/nixpkgs/issues/189851
+  systemd.user.extraConfig = ''
+    DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+  '';
 }
--- overlays/default.nix
+++ overlays/default.nix
@@ -1,3 +1,12 @@
 _: {
   additions = final: _prev: import ../packages final.pkgs;
+  modifications = _final: prev: {
+    hyprland = prev.hyprland.overrideAttrs (_old: {
+      postPatch =
+        _old.postPatch
+        + ''
+          sed -i 's|Exec=Hyprland|Exec=hypr-launch|' example/hyprland.desktop
+        '';
+    });
+  };
 }

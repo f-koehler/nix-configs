f76f10f67fd940d0a6c04fcee2ff275566721b7e
Fabian Köhler <me@fkoehler.org>
2024-12-08 17:29:24 +0800

    Only enable hyprland on Linux.

--- home/workstation/default.nix
+++ home/workstation/default.nix
@@ -1,13 +1,20 @@
-_: {
-  imports = [
-    ./alacritty.nix
-    ./email.nix
-    ./hyprland
-    # ./krohnkite.nix
-    # ./plasma.nix
-    # ./sway
-    ./vscode.nix
-    ./wezterm.nix
-    ./zed.nix
-  ];
+{
+  lib,
+  isLinux,
+  ...
+}: {
+  imports =
+    [
+      ./alacritty.nix
+      ./email.nix
+      ./vscode.nix
+      ./wezterm.nix
+      ./zed.nix
+    ]
+    ++ lib.optionals isLinux [
+      ./hyprland
+      # ./sway
+      # ./krohnkite.nix
+      # ./plasma.nix
+    ];
 }

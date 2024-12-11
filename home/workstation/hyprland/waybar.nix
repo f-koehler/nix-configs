28fb6380008ac11eca31ebe1279489b42438bf71
Fabian Köhler <me@fkoehler.org>
2024-12-11 14:58:04 +0800

    Tweak waybar config a bit.

--- home/workstation/hyprland/waybar.nix
+++ home/workstation/hyprland/waybar.nix
@@ -2,7 +2,10 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  symbolSize = "20";
+  symbolRise = "0";
+in {
   programs = {
     waybar = {
       enable = true;
@@ -19,6 +22,9 @@
           position = "top";
           modules-left = [
             "hyprland/workspaces"
+          ];
+          modules-center = [
+            "clock"
           ];
           modules-right = [
             "tray"
@@ -41,9 +47,9 @@
           };
           wireplumber = {
             scroll-step = 5;
-            format = "<big>{icon}</big>";
-            format-alt = "<big>{icon}</big> <small>{volume}󰏰</small>";
-            format-muted = "󰖁";
+            format = "<span font='${symbolSize}' rise='${symbolRise}'>{icon}</span>";
+            format-alt = "<span font='${symbolSize}' rise='${symbolRise}'>{icon}</span> {volume}󰏰";
+            format-muted = "<span font='${symbolSize}' rise='${symbolRise}'>󰖁</span>";
             format-icons = {
               default = [
                 "󰕿"
@@ -52,30 +58,30 @@
               ];
             };
             max-volume = 100;
-            on-click-middle = "${pkgs.avizo}/bin/volumectl toggle-mute";
-            on-click-right = "hyprctl dispatch exec [workspace current] ${lib.getExe pkgs.pwvucontrol}";
-            on-scroll-up = "${pkgs.avizo}/bin/volumectl -u up 2";
-            on-scroll-down = "${pkgs.avizo}/bin/volumectl -u down 2";
+            on-click-middle = "${pkgs.swayosd}/bin/swayosd-client --output-volume mute-toggle";
+            on-click-right = "${pkgs.hyprland}/bin/hyprctl dispatch exec [workspace current] ${lib.getExe pkgs.pwvucontrol}";
+            on-scroll-up = "${pkgs.swayosd}/bin/swayosd-client --output-volume raise";
+            on-scroll-down = "${pkgs.swayosd}/bin/swayosd-client --output-volume lower";
             tooltip-format = "󰓃  {volume}󰏰\n󰒓  {node_name}";
           };
           pulseaudio = {
-            format = "<big>{format_source}</big>";
-            format-alt = "<big>{format_source}</big> <small>{source_volume}󰏰</small>";
-            format-source = "󰍰";
-            format-source-muted = "󰍱";
-            on-click-middle = "${pkgs.avizo}/bin/volumectl -m toggle-mute";
-            on-click-right = "hyprctl dispatch exec [workspace current] ${lib.getExe pkgs.pwvucontrol}";
-            on-scroll-up = "${pkgs.avizo}/bin/volumectl -m up 2";
-            on-scroll-down = "${pkgs.avizo}/bin/volumectl -m down 2";
+            format = "{format_source}";
+            format-alt = "{format_source} {source_volume}󰏰";
+            format-source = "<span font='${symbolSize}' rise='${symbolRise}'>󰍰</span>";
+            format-source-muted = "<span font='${symbolSize}' rise='${symbolRise}'>󰍱</span>";
+            on-click-middle = "${pkgs.swayosd}/bin/swayosd-client --input-volume mute-toggle";
+            on-click-right = "${pkgs.hyprland}/bin/hyprctl dispatch exec [workspace current] ${lib.getExe pkgs.pwvucontrol}";
+            on-scroll-up = "${pkgs.swayosd}/bin/swayosd-client --input-volume raise";
+            on-scroll-down = "${pkgs.swayosd}/bin/swayosd-client --input-volume lower";
             tooltip-format = "  {source_volume}󰏰\n󰒓  {desc}";
           };
           network = {
             format = "<big>{icon}</big>";
             format-alt = " <small>{bandwidthDownBits}</small>  <small>{bandwidthUpBits}</small>";
-            format-ethernet = "󰈀";
-            format-disconnected = "󱚵";
-            format-linked = "";
-            format-wifi = "󰖩";
+            format-ethernet = "<span font='${symbolSize}' rise='${symbolRise}'>󰈀</span>";
+            format-disconnected = "<span font='${symbolSize}' rise='${symbolRise}'>󱚵</span>";
+            format-linked = "<span font='${symbolSize}' rise='${symbolRise}'></span>";
+            format-wifi = "<span font='${symbolSize}' rise='${symbolRise}'>󰖩</span>";
             interval = 2;
             on-click-right = "hyprctl dispatch exec [workspace current] ${pkgs.networkmanagerapplet}/bin/nm-connection-editor";
             tooltip-format = "  {ifname}\n󰩠  {ipaddr} via {gwaddr}\n  {bandwidthDownBits}\t  {bandwidthUpBits}";
@@ -84,11 +90,11 @@
             tooltip-format-disconnected = "󱚵  disconnected";
           };
           bluetooth = {
-            format = "<big>{icon}</big>";
-            format-connected = "󰂱";
-            format-disabled = "󰂲";
-            format-on = "󰂯";
-            format-off = "󰂲";
+            format = "<span font='${symbolSize}' rise='${symbolRise}'>󰂯</span>";
+            format-connected = "<span font='${symbolSize}' rise='${symbolRise}'>󰂯</span>";
+            format-disabled = "<span font='${symbolSize}' rise='${symbolRise}'>󰂲</span>";
+            format-on = "<span font='${symbolSize}' rise='${symbolRise}'>󰂯</span>";
+            format-off = "<span font='${symbolSize}' rise='${symbolRise}'>󰂲</span>";
             #on-click-middle = "${lib.getExe bluetoothToggle}";
             on-click-right = "hyprctl dispatch exec [workspace current] ${pkgs.blueberry}/bin/blueberry";
             tooltip-format = "  {controller_alias}\t󰿀  {controller_address}\n󰂴  {num_connections} connected";
@@ -103,7 +109,7 @@
       style = ''
         * {
           font-family: Cascadia Code NF;
-          font-size: 22px;
+          font-size: 18px;
           min-height: 0;
         }
       '';

f95d2dafd442b703d64767e0bc3a16cbf9f79f30
Fabian Köhler <me@fkoehler.org>
2024-12-13 01:06:09 +0800

    Configure pulling of snapshots from homeserver to vps.

--- nixos/configs/vps.nix
+++ nixos/configs/vps.nix
@@ -1,4 +1,9 @@
-{modulesPath, ...}: {
+{
+  config,
+  modulesPath,
+  pkgs,
+  ...
+}: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
@@ -11,10 +16,55 @@
   };
   services.openssh.enable = true;
 
-  # environment.systemPackages = map lib.lowPrio [
-  #   pkgs.curl
-  #   pkgs.gitMinimal
-  # ];
+  environment.systemPackages = [
+    pkgs.curl
+    pkgs.git
+    pkgs.mbuffer # for syncoid
+    pkgs.lzop # for syncoid
+  ];
+
+  sops = {
+    secrets = {
+      "syncoid/ssh_key" = {
+        owner = config.services.syncoid.user;
+        group = config.services.syncoid.group;
+      };
+    };
+  };
+
+  services = {
+    syncoid = {
+      enable = true;
+      commonArgs = ["--no-sync-snap"];
+      interval = "daily";
+      sshKey = config.sops.secrets."syncoid/ssh_key".path;
+      commands = {
+        "homeserver-paperless" = {
+          source = "root@homeserver:rpool/paperless";
+          target = "rpool/backups/homeserver/paperless";
+        };
+        "homeserver-nextcloud" = {
+          source = "root@homeserver:rpool/nextcloud";
+          target = "rpool/backups/homeserver/nextcloud";
+        };
+        "homeserver-postgresql" = {
+          source = "root@homeserver:rpool/postgresql";
+          target = "rpool/backups/homeserver/postgresql";
+        };
+      };
+    };
+    zfs = {
+      trim = {
+        enable = true;
+        interval = "weekly";
+      };
+      autoScrub = {
+        enable = true;
+        interval = "weekly";
+      };
+      autoSnapshot.enable = false;
+    };
+  };
 
   networking.hostId = "168fda19";
 }

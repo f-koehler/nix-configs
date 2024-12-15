fcc7e256403df6a64c5ff2364540d2527c829b1d
Fabian Koehler <fabian.koehler@proton.me>
2024-12-15 16:54:32 +0800

    Add battery script for sketchybar.

--- packages/sketchybar-plugins/default.nix
+++ packages/sketchybar-plugins/default.nix
@@ -5,6 +5,7 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/bin
     cp aerospace.sh $out/bin
+    cp battery.sh $out/bin
     cp front-app.sh $out/bin
     cp clock.sh $out/bin
     cp volume.sh $out/bin

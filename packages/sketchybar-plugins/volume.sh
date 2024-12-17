d160491c972ad69949b9c1ca08cea73d93ab0a07
Fabian Koehler <fabian.koehler@proton.me>
2024-12-17 23:17:24 +0800

    Remove icon padding from sketchybar volume plugin.

--- packages/sketchybar-plugins/volume.sh
+++ packages/sketchybar-plugins/volume.sh
@@ -3,16 +3,13 @@
 case ${INFO} in
 0)
 	ICON=""
-	ICON_PADDING_RIGHT=21
 	;;
 [0-9])
 	ICON=""
-	ICON_PADDING_RIGHT=12
 	;;
 *)
 	ICON=""
-	ICON_PADDING_RIGHT=6
 	;;
 esac
 
-sketchybar --set "$NAME" icon="$ICON" icon.padding_right="$ICON_PADDING_RIGHT" label="$INFO%"
+sketchybar --set "$NAME" icon="$ICON" label="$INFO%"

847228666d835009576b59fc32e3a4f46dc06274
Fabian Koehler <fabian.koehler@proton.me>
2024-12-18 01:08:40 +0800

    Package sketchybar lua plugin.

--- packages/default.nix
+++ packages/default.nix
@@ -1,3 +1,4 @@
 pkgs: {
+  sketchybar-lua = pkgs.callPackage ./sketchybar-lua {};
   sketchybar-plugins = pkgs.callPackage ./sketchybar-plugins {};
 }
---
+++ packages/sketchybar-lua/default.nix
@@ -0,0 +1,32 @@
+{
+  stdenv,
+  fetchFromGitHub,
+  gcc,
+  readline,
+  ...
+}:
+stdenv.mkDerivation {
+  name = "sketchybar-lua";
+  src = fetchFromGitHub {
+    owner = "FelixKratz";
+    repo = "SbarLua";
+    rev = "refs/heads/master";
+    sha256 = "sha256-F0UfNxHM389GhiPQ6/GFbeKQq5EvpiqQdvyf7ygzkPg=";
+  };
+
+  buildInputs = [gcc readline.dev];
+
+  patchPhase = ''
+    substituteInPlace Makefile \
+      --replace '$(HOME)/.local/share/sketchybar_lua' '$out'
+  '';
+
+  buildPhase = ''
+    make
+  '';
+
+  installPhase = ''
+    mkdir -p $out/lib
+    cp bin/sketchybar.so $out/lib
+  '';
+}

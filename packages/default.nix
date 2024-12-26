28d899cac79936775a4ff9150c15443a67653863
Fabian Koehler <fabian.koehler@proton.me>
2024-12-27 05:08:38 +0800

    Simplify packages nix file.

--- packages/default.nix
+++ packages/default.nix
@@ -1,9 +1,5 @@
-pkgs: let
+pkgs: rec {
   sketchybar-lua = pkgs.callPackage ./sketchybar-lua {};
   sketchybar-config = pkgs.callPackage ./sketchybar-config {inherit sketchybar-lua;};
   sketchybar-plugins = pkgs.callPackage ./sketchybar-plugins {};
-in {
-  inherit sketchybar-config;
-  inherit sketchybar-lua;
-  inherit sketchybar-plugins;
 }

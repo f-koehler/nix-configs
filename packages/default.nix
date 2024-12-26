6ec6da0d516cdefab018af21950c285d58f159f4
Fabian Koehler <fabian.koehler@proton.me>
2024-12-27 05:09:30 +0800

    Remove deprecated sketchybar-plugins package.

--- packages/default.nix
+++ packages/default.nix
@@ -1,5 +1,4 @@
 pkgs: rec {
   sketchybar-lua = pkgs.callPackage ./sketchybar-lua {};
   sketchybar-config = pkgs.callPackage ./sketchybar-config {inherit sketchybar-lua;};
-  sketchybar-plugins = pkgs.callPackage ./sketchybar-plugins {};
 }

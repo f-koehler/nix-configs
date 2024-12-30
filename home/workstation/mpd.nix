fdf3a4ec284ef231c1bcf9fda5b367fa3ebafa7b
Fabian Koehler <fabian.koehler@proton.me>
2024-12-31 00:13:44 +0800

    Switch to remote MPD server.

--- home/workstation/mpd.nix
+++ home/workstation/mpd.nix
@@ -1,21 +1,37 @@
-{
-  pkgs,
-  nodeConfig,
-  ...
-}: {
+_: let
+  mpdHost = "100.64.220.85";
+in {
   services = {
-    mpd = {
-      enable = true;
-      musicDirectory = "/home/${nodeConfig.username}/Network/media1/soundtracks";
-      network = {
-        startWhenNeeded = true;
-      };
-    };
     mpd-mpris = {
       enable = true;
+      mpd = {
+        host = "${mpdHost}";
+        useLocal = false;
+      };
     };
   };
-  home.packages = [
-    pkgs.ncmpcpp
-  ];
+  programs.ncmpcpp = {
+    enable = true;
+    settings = {
+      mpd_host = "${mpdHost}";
+    };
+    bindings = [
+      {
+        key = "j";
+        command = "scroll_down";
+      }
+      {
+        key = "k";
+        command = "scroll_up";
+      }
+      {
+        key = "J";
+        command = ["select_item" "scroll_down"];
+      }
+      {
+        key = "K";
+        command = ["select_item" "scroll_up"];
+      }
+    ];
+  };
 }

02705aef322b75e985a4e0c460d26c6138077c9a
Fabian Koehler <fabian.koehler@proton.me>
2025-01-24 10:52:16 +0800

    Disable currently non-working codeium plugin for nvim.

--- home/common/neovim/plugins/codeium.nix
+++ home/common/neovim/plugins/codeium.nix
@@ -1,19 +1,21 @@
-{
-  lib,
-  pkgs,
-  nodeConfig,
-  ...
-}:
-{
-  programs.nixvim.plugins.codeium-nvim = {
-    enable = nodeConfig.isWorkstation;
-    settings = {
-      tools = {
-        curl = lib.getExe pkgs.curl;
-        gzip = lib.getExe pkgs.gzip;
-        uname = lib.getExe' pkgs.coreutils "uname";
-        uuidgen = lib.getExe' pkgs.util-linux "uuidgen";
-      };
-    };
-  };
-}
+_: { }
+
+# {
+#   lib,
+#   pkgs,
+#   nodeConfig,
+#   ...
+# }:
+# {
+#   programs.nixvim.plugins.codeium-nvim = {
+#     enable = nodeConfig.isWorkstation;
+#     settings = {
+#       tools = {
+#         curl = lib.getExe pkgs.curl;
+#         gzip = lib.getExe pkgs.gzip;
+#         uname = lib.getExe' pkgs.coreutils "uname";
+#         uuidgen = lib.getExe' pkgs.util-linux "uuidgen";
+#       };
+#     };
+#   };
+# }

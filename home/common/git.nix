1f34b67a811d6d27a90e68ab8ff69327f33f2c23
Fabian Koehler <fabian.koehler@proton.me>
2025-01-27 10:25:57 +0800

    Install lazygit in home configurations.

--- home/common/git.nix
+++ home/common/git.nix
@@ -1,22 +1,27 @@
 _: {
-  programs.git = {
-    enable = true;
-    userEmail = "fabian.koehler@proton.me";
-    userName = "Fabian Koehler";
-    signing = {
-      key = "C5DC80511469AD81C84E3564D55A35AFB2900A11";
+  programs = {
+    git = {
+      enable = true;
+      userEmail = "fabian.koehler@proton.me";
+      userName = "Fabian Koehler";
+      signing = {
+        key = "C5DC80511469AD81C84E3564D55A35AFB2900A11";
+      };
+      delta.enable = true;
+      extraConfig = {
+        pull = {
+          rebase = "false";
+        };
+        init = {
+          defaultBranch = "main";
+        };
+        color = {
+          ui = "auto";
+        };
+      };
     };
-    delta.enable = true;
-    extraConfig = {
-      pull = {
-        rebase = "false";
-      };
-      init = {
-        defaultBranch = "main";
-      };
-      color = {
-        ui = "auto";
-      };
+    lazygit = {
+      enable = true;
     };
   };
 }

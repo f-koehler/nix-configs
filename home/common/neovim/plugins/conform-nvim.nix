7859139c933e9e164b6ad3d4ac491d46eccca2c8
Fabian Koehler <fabian.koehler@proton.me>
2025-02-07 17:55:32 +0800

    Restart configuring conform-nvim.

--- home/common/neovim/plugins/conform-nvim.nix
+++ home/common/neovim/plugins/conform-nvim.nix
@@ -1,73 +1,17 @@
-{
-  lib,
-  pkgs,
-  ...
-}:
-{
+_: {
   programs.nixvim = {
     plugins.conform-nvim = {
       enable = true;
       settings = {
-        log_level = "warn";
+        format_on_save = {
+          lsp_fallback = "fallback";
+          timeout_ms = 500;
+        };
         notify_on_error = true;
-        notify_no_formatters = true;
         formatters_by_ft = {
-          bash = [
-            "shellcheck"
-            "shellharden"
-            "shfmt"
-          ];
           cpp = [ "clang-format" ];
-          nix = [ "nixfmt" ];
-          python = [ "ruff" ];
-          rust = [ "rustfmt" ];
+          python = [ "ruff_format" ];
         };
-        formatters = {
-          nixfmt = {
-            command = lib.getExe pkgs.nixfmt-rfc-style;
-          };
-          clang-format = {
-            command = "${pkgs.clang-tools}/bin/clang-format";
-            args = [
-              "-i"
-              "$FILENAME"
-            ];
-          };
-          ruff = {
-            command = lib.getExe pkgs.ruff;
-            args = [
-              "format"
-              "$FILENAME"
-            ];
-          };
-          rustfmt = {
-            command = lib.getExe pkgs.rustfmt;
-          };
-          shellcheck = {
-            command = lib.getExe pkgs.shellcheck;
-          };
-          shfmt = {
-            command = lib.getExe pkgs.shfmt;
-          };
-          shellharden = {
-            command = lib.getExe pkgs.shellharden;
-          };
-        };
-        format_on_save =
-          # Lua
-          ''
-            {
-                lsp_format = "fallback",
-                timeout_ms = 500,
-            }
-          '';
-        format_after_save =
-          # Lua
-          ''
-            {
-                  lsp_format = "fallback",
-            }
-          '';
       };
     };
   };

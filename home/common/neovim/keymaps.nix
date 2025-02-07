ee0b6e39ff9c0d89841e0c94ddd03c6326a44804
Fabian Koehler <fabian.koehler@proton.me>
2025-02-07 17:49:22 +0800

    Setup some LSP keymaps in neovim.

--- home/common/neovim/keymaps.nix
+++ home/common/neovim/keymaps.nix
@@ -1,22 +1,4 @@
 _: {
   programs.nixvim.keymaps = [
-    {
-      action = ":lua vim.lsp.buf.definition()<CR>";
-      key = "gd";
-      mode = "n";
-      options = {
-        silent = true;
-        desc = "Go to definition.";
-      };
-    }
-    {
-      action = ":lua vim.lsp.buf.declaration()<CR>";
-      key = "gD";
-      mode = "n";
-      options = {
-        silent = true;
-        desc = "Go to declaration.";
-      };
-    }
   ];
 }
--- home/common/neovim/plugins/lsp.nix
+++ home/common/neovim/plugins/lsp.nix
@@ -1,4 +1,7 @@
-{ nodeConfig, ... }:
+{
+  nodeConfig,
+  ...
+}:
 {
   programs.nixvim.plugins.lsp = {
     enable = true;
@@ -9,10 +12,23 @@
       gopls.enable = nodeConfig.isWorkstation;
       nil_ls.enable = true;
       pyright.enable = nodeConfig.isWorkstation;
+      qmlls = {
+        enable = nodeConfig.isWorkstation;
+        # cmd = [ "${lib.getExe' pkgs.kdePackages.qtdeclarative "qmlls"}" "-E" ];
+      };
       rust_analyzer = {
         enable = nodeConfig.isWorkstation;
         installCargo = false;
         installRustc = false;
+      };
+    };
+    keymaps = {
+      lspBuf = {
+        "gd" = "definition";
+        "gD" = "declaration";
+        "gr" = "references";
+        "<leader>la" = "code_action";
+        "<leader>lr" = "rename";
       };
     };
   };

d070be547f3b13ff56bc7bcff4d23e319cfaab5a
Fabian Koehler <fabian.koehler@proton.me>
2025-03-06 18:43:16 +0800

    Work on a transmission config.

--- nixos/configs/homeserver/transmission.nix
+++ nixos/configs/homeserver/transmission.nix
@@ -25,6 +25,8 @@
         TZ=Asia/Singapore
         USER=fkoehler
         PASSWORD=${config.sops.placeholder."services/transmission/password"}
+        WHITELIST=*.*.*.*
+        HOST_WHITELIST=torrent.fkoehler.xyz
       '';
     };
   };
@@ -66,6 +68,43 @@
       volumes = [
         "/var/lib/transmission/app:/config:rw"
       ];
+    };
+  };
+  services.nginx = {
+    upstreams."transmission" = {
+      servers = {
+        "127.0.0.1:9091" = { };
+      };
+    };
+    virtualHosts."torrent.fkoehler.xyz" = {
+      forceSSL = true;
+      kTLS = true;
+      sslCertificate = "/var/lib/acme/fkoehler.xyz/fullchain.pem";
+      sslCertificateKey = "/var/lib/acme/fkoehler.xyz/key.pem";
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 443;
+          ssl = true;
+        }
+      ];
+      locations = {
+        "/" = {
+          proxyPass = "http://transmission/";
+          extraConfig = ''
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "upgrade";
+
+            proxy_redirect off;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Host $server_name;
+            add_header Referrer-Policy "strict-origin-when-cross-origin";
+          '';
+        };
+      };
     };
   };
 }

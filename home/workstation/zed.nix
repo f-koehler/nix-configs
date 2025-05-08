af560bd2b2d348ae641d883dea9d28debd8693ff
Fabian Koehler <fabian.koehler@proton.me>
2025-05-09 00:09:38 +0800

    Update zed config.

--- home/workstation/zed.nix
+++ home/workstation/zed.nix
@@ -1,10 +1,16 @@
 _: {
   programs.zed-editor = {
-    enable = false;
-    extensions = [ "nix" ];
+    enable = true;
+    installRemoteServer = true;
+    extensions = [
+      "env"
+      "neocmake"
+      "nix"
+    ];
     userSettings = {
       buffer_font_family = "Cascadia Code NF";
       buffer_font_size = 12;
+      load_direnv = "shell_hook";
       telemetry.metrics = false;
       ui_font_size = 14;
       vim_mode = true;

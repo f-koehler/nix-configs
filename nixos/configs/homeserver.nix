47b0b52aa6e9af588f21d862899d49c3d8eea1da
Fabian Koehler <fabian.koehler@proton.me>
2025-05-16 15:05:24 +0800

    Configure homeserver static IP.

--- nixos/configs/homeserver.nix
+++ nixos/configs/homeserver.nix
@@ -42,6 +42,19 @@
     autoSnapshot.enable = false;
   };
 
+  networking = {
+    interfaces."enp3s0".ipv4.addresses = [
+      {
+        address = "192.168.1.92";
+        prefixLength = 24;
+      }
+    ];
+    defaultGateway = {
+      address = "192.168.1.1";
+      interface = "enp3s0";
+    };
+  };
+
   users = {
     groups = {
       media = {

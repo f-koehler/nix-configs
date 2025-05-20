30d650c8044a8a1472e02f4fcdf4ba18e062646e
Fabian Koehler <fabian.koehler@proton.me>
2025-05-21 01:01:05 +0800

    Continue preparing immich backup mechanism.

--- nixos/configs/homeserver/immich.nix
+++ nixos/configs/homeserver/immich.nix
@@ -1,6 +1,4 @@
 {
-  pkgs,
-  lib,
   config,
   nodeConfig,
   stateVersion,
@@ -9,19 +7,11 @@
 let
   ip = "172.22.1.100";
   inherit (config.services.immich) port;
-  backupScript = pkgs.writeShellScriptBin "immich-backup-db" ''
-    ${lib.getExe' pkgs.nixos-container "nixos-container"} run immich -- sh -c "sudo -u immich -g immich pg_dump -d immich | gzip -9 > /db_backup/immich.psql.gz"
-  '';
+  # backupScript = pkgs.writeShellScriptBin "immich-backup-db" ''
+  #   ${lib.getExe' pkgs.nixos-container "nixos-container"} run immich -- sh -c "sudo -u immich -g immich pg_dump -d immich | gzip -9 > /db_backup/immich.psql.gz"
+  # '';
 in
 {
-  systemd.services.immich-backup-db = {
-    wantedBy = [ "multi-user.target" ];
-    description = "Create backup of Immich PostgreSQL database";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${lib.getExe backupScript}";
-    };
-  };
   containers.immich = {
     autoStart = true;
     privateNetwork = true;
@@ -63,38 +53,55 @@ in
       };
     };
   };
-  services.nginx = {
-    upstreams."immich" = {
-      servers = {
-        "${ip}:${toString port}" = { };
+  services = {
+    nginx = {
+      upstreams."immich" = {
+        servers = {
+          "${ip}:${toString port}" = { };
+        };
       };
-    };
-    virtualHosts."photos.fkoehler.xyz" = {
-      forceSSL = true;
-      kTLS = true;
-      sslCertificate = "/var/lib/acme/fkoehler.xyz/fullchain.pem";
-      sslCertificateKey = "/var/lib/acme/fkoehler.xyz/key.pem";
-      listen = [
-        {
-          addr = "0.0.0.0";
-          port = 443;
-          ssl = true;
-        }
-      ];
-      locations = {
-        "/" = {
-          proxyPass = "http://immich/";
-          extraConfig = ''
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header Host $host;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "upgrade";
-            proxy_http_version 1.1;
-            proxy_redirect http:// https://;
-          '';
+      virtualHosts."photos.fkoehler.xyz" = {
+        forceSSL = true;
+        kTLS = true;
+        sslCertificate = "/var/lib/acme/fkoehler.xyz/fullchain.pem";
+        sslCertificateKey = "/var/lib/acme/fkoehler.xyz/key.pem";
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 443;
+            ssl = true;
+          }
+        ];
+        locations = {
+          "/" = {
+            proxyPass = "http://immich/";
+            extraConfig = ''
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+              proxy_set_header Host $host;
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection "upgrade";
+              proxy_http_version 1.1;
+              proxy_redirect http:// https://;
+            '';
+          };
         };
       };
     };
+    # sanoid = {
+    #   enable = true;
+    #   datasets.immich = {
+    #     yearly = 2;
+    #     monthly = 24;
+    #     daily = 90;
+    #     hourly = 96;
+    #     autosnap = true;
+    #     autoprune = true;
+    #     pre_snapshot_script = "${lib.getExe backupScript}";
+    #     no_inconsistent_snapshot = true;
+    #     script_timeout = 0;
+    #     recursive = true;
+    #   };
+    # };
   };
 }

48a23ef555e804fa45b2797d3f418ae252a25902
Fabian Koehler <fabian.koehler@proton.me>
2025-05-31 10:40:23 +0800

    chore: determine bat path from config

--- home/common/aliases.nix
+++ home/common/aliases.nix
@@ -9,7 +9,7 @@
     "cp" = "cp -i";
     "mv" = "mv -i";
     "rm" = "rm -i";
-    "cat" = "bat --style=plain --paging=never";
+    "cat" = "${lib.getExe' config.programs.bat.package "bat"} --style=plain --paging=never";
     "open" = lib.mkIf isLinux "xdg-open";
     "lg" = "${lib.getExe' config.programs.lazygit.package "lazygit"}";
   };

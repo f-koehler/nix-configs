35bd4c76415ebc78d1b891ef4709472c73726ead
Fabian Koehler <fabian.koehler@proton.me>
2025-05-31 10:45:27 +0800

    fix: make awscli work on untrusted nodes

--- home/common/aws.nix
+++ home/common/aws.nix
@@ -5,18 +5,21 @@
   nodeConfig,
   ...
 }:
-lib.mkIf nodeConfig.isTrusted {
+{
   sops.secrets = {
     "aws/key_id".path = "${config.home.homeDirectory}/.local/share/aws/key_id";
     "aws/key".path = "${config.home.homeDirectory}/.local/share/aws/key";
   };
-  home.packages =
-    let
-      awscli-wrapped = pkgs.writeShellScriptBin "aws" ''
-        export AWS_ACCESS_KEY_ID=$(cat ${config.sops.secrets."aws/key_id".path})
-        export AWS_SECRET_ACCESS_KEY=$(cat ${config.sops.secrets."aws/key".path})
-        ${pkgs.awscli}/bin/aws "$@"
-      '';
-    in
-    [ awscli-wrapped ];
+  programs.awscli = {
+    enable = true;
+    package =
+      if nodeConfig.isTrusted then
+        pkgs.writeShellScriptBin "aws" ''
+          export AWS_ACCESS_KEY_ID=$(cat ${config.sops.secrets."aws/key_id".path})
+          export AWS_SECRET_ACCESS_KEY=$(cat ${config.sops.secrets."aws/key".path})
+          ${lib.getExe' pkgs.awscli2 "aws"} "$@"
+        ''
+      else
+        pkgs.awscli2;
+  };
 }

b500ec6eb31ae02fdd677a6fd96acb90c95f21e3
Fabian Koehler <fabian.koehler@proton.me>
2025-06-01 13:15:09 +0800

    feat: configure font for rofi

--- home/workstation/sway/rofi.nix
+++ home/workstation/sway/rofi.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  nodeConfig,
+  ...
+}:
 {
   catppuccin.rofi = {
     enable = true;
@@ -7,6 +12,7 @@
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
+    font = "${builtins.elemAt config.fonts.fontconfig.defaultFonts.sansSerif 0} ${toString nodeConfig.fontSize}";
     extraConfig = {
       show-icons = true;
     };

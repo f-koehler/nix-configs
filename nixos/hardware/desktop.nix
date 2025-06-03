a706c6158359317a4abfa4fb83c0b9ada3d15526
Fabian Koehler <fabian.koehler@proton.me>
2025-06-03 10:14:08 +0800

    refactor: remove zenpower module as it seems to be outdated and broken

--- nixos/hardware/desktop.nix
+++ nixos/hardware/desktop.nix
@@ -4,7 +4,6 @@
     (modulesPath + "/installer/scan/not-detected.nix")
     inputs.nixos-hardware.nixosModules.common-cpu-amd
     inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-    inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
     inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
     inputs.nixos-hardware.nixosModules.common-hidpi
     inputs.nixos-hardware.nixosModules.common-pc

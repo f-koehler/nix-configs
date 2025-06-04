d4919e4930e7313ef9d381b57fa574cf706c2d48
Fabian Koehler <fabian.koehler@proton.me>
2025-06-04 23:07:01 +0800

    refactor!: only configure qt theming when not on Plasma

--- home/workstation/qt.nix
+++ home/workstation/qt.nix
@@ -1,9 +1,14 @@
-_: {
-  qt = {
-    enable = true;
+{ nodeConfig, lib, ... }:
+let
+  hasPlasma = builtins.elem "plasma" nodeConfig.desktops;
+in
+{
+  catppuccin.kvantum.enable = false;
+  qt = lib.mkIf hasPlasma {
+    enable = !hasPlasma;
 
     # set theme/style to kvantum for catppuccin
-    platformTheme.name = "kvantum";
+    platformTheme.name = "qtct";
     style.name = "kvantum";
   };
 }

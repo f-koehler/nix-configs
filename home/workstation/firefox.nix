e2d640357b01353cb5e7eef4c721f5d954f635c2
Fabian Koehler <fabian.koehler@proton.me>
2025-06-25 18:44:22 +0800

    feat: install firefox extensions via home-manager

--- home/workstation/firefox.nix
+++ home/workstation/firefox.nix
@@ -1,5 +1,17 @@
-_: {
+_:
+let
+  extensionsToInstall = {
+    "bitwarden" = "{446900e4-71c2-419f-a6a7-df9c091e268b}";
+    "consent-o-matic" = "gdpr@cavi.au.dk";
+    "dark-reader" = "addon@darkreader.org";
+    "raindrop" = "jid0-adyhmvsP91nUO8pRv0Mn2VKeB84@jetpack";
+    "ublock-origin" = "uBlock0@raymondhill.net";
+    "zotero-connector" = "zotero@chnm.gmu.edu";
+  };
+in
+{
   programs.firefox = {
+    enable = true;
     profiles.default = {
       settings = {
         # UI
@@ -8,6 +20,13 @@ _: {
 
         "extensions.pocket.enabled" = false;
       };
+    };
+    policies = {
+      # Browser Extensions
+      ExtensionSettings = builtins.mapAttrs (_: slug: {
+        installation_mode = "force_installed";
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/${slug}/latest.xpi";
+      }) extensionsToInstall;
     };
   };
 }

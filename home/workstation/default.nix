da336b8394faf58ab8edbfc951a6246df8ea9416
Fabian Koehler <fabian.koehler@proton.me>
2025-06-26 14:58:56 +0800

    feat: configure stylix icon theme

--- home/workstation/default.nix
+++ home/workstation/default.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   nodeConfig,
   ...
@@ -19,4 +20,11 @@
     ]
     ++ lib.optionals (builtins.elem "sway" nodeConfig.desktops) [ ./sway ]
     ++ lib.optionals (builtins.elem "plasma" nodeConfig.desktops) [ ./plasma.nix ];
+
+  stylix.iconTheme = {
+    enable = true;
+    light = "breeze";
+    dark = "breeze-dark";
+    package = pkgs.kdePackages.breeze-icons;
+  };
 }

dcad841d5e8c1a39f22919482289f9fa95f40fc7
Fabian Koehler <fabian.koehler@proton.me>
2025-06-26 18:02:05 +0800

    refactor: do not use wallpaper for grub

--- nixos/modules/workstation/boot.nix
+++ nixos/modules/workstation/boot.nix
@@ -8,7 +8,6 @@
   stylix.targets = {
     grub = {
       inherit (config.boot.loader.grub) enable;
-      useWallpaper = true;
     };
     plymouth = {
       inherit (config.boot.plymouth) enable;

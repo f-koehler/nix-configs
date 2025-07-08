55dcf5205956191ef29ae4c888e539b5830d980d
Fabian Koehler <fabian.koehler@proton.me>
2025-07-08 11:27:13 +0800

    feat(homepage): start integrating services into dashboard

--- nixos/configs/homeserver/containers/homepage.nix
+++ nixos/configs/homeserver/containers/homepage.nix
@@ -23,10 +23,14 @@ libContainer.mkContainer rec {
   inherit name ip;
   hostName = "homepage";
   port = config.services.homepage-dashboard.listenPort;
+  bindMounts = {
+    "${config.sops.templates."homepage-dashboard.env".path}".isReadOnly = true;
+  };
   extraConfig = _: {
     services.homepage-dashboard = {
       enable = true;
       listenPort = port;
+      environmentFile = "${config.sops.templates."homepage-dashboard.env".path}";
       allowedHosts = "127.0.0.1:${toString port},localhost:${toString port},${hostName}.${nodeConfig.domain}";
       services = [
         {
@@ -40,11 +44,11 @@ libContainer.mkContainer rec {
                   icon = "audiobookshelf.svg";
                   href = url;
                   description = "Audiobooks and podcasts";
-                  # widget = {
-                  #   type = "audiobookshelf";
-                  #   inherit url;
-                  #   key = "{{HOMEPAGE_VAR_AUDIOBOOKSHELF_API_KEY}}";
-                  # };
+                  widget = {
+                    type = "audiobookshelf";
+                    inherit url;
+                    key = "{{HOMEPAGE_VAR_AUDIOBOOKSHELF_KEY}}";
+                  };
                 };
             }
             {
@@ -56,13 +60,13 @@ libContainer.mkContainer rec {
                   icon = "navidrome.svg";
                   href = url;
                   description = "Music";
-                  # widget = {
-                  #   type = "navidrome";
-                  #   inherit url;
-                  # user = "";
-                  #   token = "{{HOMEPAGE_VAR_AUDIOBOOKSHELF_API_KEY}}";
-                  #   randomsalt = "";
-                  # };
+                  widget = {
+                    type = "navidrome";
+                    inherit url;
+                    user = "fkoehler";
+                    token = "{{HOMEPAGE_VAR_NAVIDROME_KEY}}";
+                    randomsalt = "{{HOMEPAGE_VAR_NAVIDROME_SALT}}";
+                  };
                 };
             }
             {
@@ -74,18 +78,38 @@ libContainer.mkContainer rec {
                   icon = "immich.svg";
                   href = url;
                   description = "Photos";
-                  # widget = {
-                  #   type = "immich";
-                  #   inherit url;
-                  #   key = "{{HOMEPAGE_VAR_AUDIOBOOKSHELF_API_KEY}}";
-                  #   version = 2;
-                  # };
+                  widget = {
+                    type = "immich";
+                    inherit url;
+                    key = "{{HOMEPAGE_VAR_IMMICH_KEY}}";
+                    version = 2;
+                  };
                 };
             }
           ];
         }
         # TODO(fk): add stirling PDF to productivity group
       ];
+    };
+  };
+}
+// {
+  sops = {
+    secrets = {
+      "services/homepage/services/audiobookshelf/key" = { };
+      "services/homepage/services/immich/key" = { };
+      "services/homepage/services/navidrome/key" = { };
+      "services/homepage/services/navidrome/salt" = { };
+    };
+    templates."homepage-dashboard.env" = {
+      content = ''
+        HOMEPAGE_VAR_AUDIOBOOKSHELF_KEY=${
+          config.sops.placeholder."services/homepage/services/audiobookshelf/key"
+        }
+        HOMEPAGE_VAR_IMMICH_KEY=${config.sops.placeholder."services/homepage/services/immich/key"}
+        HOMEPAGE_VAR_NAVIDROME_KEY=${config.sops.placeholder."services/homepage/services/navidrome/key"}
+        HOMEPAGE_VAR_NAVIDROME_SALT=${config.sops.placeholder."services/homepage/services/navidrome/salt"}
+      '';
     };
   };
 }

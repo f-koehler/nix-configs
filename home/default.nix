e1ea35a02c9ccd16d1b3ace7020bde1007cd855c
Fabian Koehler <fabian.koehler@proton.me>
2025-08-13 20:57:14 +0800

    refactor(home)!: clean up main home-manager file

--- home/default.nix
+++ home/default.nix
@@ -4,9 +4,7 @@
   inputs,
   outputs,
   stateVersion,
-  isLinux,
   nodeConfig,
-  config,
   ...
 }:
 let
@@ -23,7 +21,8 @@ in
     ../stylix.nix
     ./common
     ./secrets.nix
-  ] ++ lib.optional nodeConfig.isWorkstation ./workstation;
+  ]
+  ++ lib.optional nodeConfig.isWorkstation ./workstation;
 
   nixpkgs = {
     overlays = [
@@ -48,61 +47,42 @@ in
       if isDarwin then "/Users/${nodeConfig.username}" else "/home/${nodeConfig.username}";
 
     packages = [
-      pkgs.git
       pkgs.age
-      pkgs.bat
-      pkgs.du-dust
-      pkgs.fish
-      pkgs.jq
-      pkgs.pre-commit
-      pkgs.rye
-      pkgs.sops
-      pkgs.zsh
-      pkgs.pandoc
       pkgs.aria
-      pkgs.onefetch
-      pkgs.gdu
-      pkgs.hyperfine
-      pkgs.yq-go
-      pkgs.ncdu
-      pkgs.htop
       pkgs.cascadia-code
-      pkgs.noto-fonts
       pkgs.fd
-      pkgs.typst
-    ] ++ lib.optionals isLinux [ pkgs.isd ];
+      pkgs.hyperfine
+      pkgs.jq
+      pkgs.ncdu
+      pkgs.noto-fonts
+      pkgs.pandoc
+      pkgs.pre-commit
+      pkgs.sops
+    ];
 
     # Home Manager is pretty good at managing dotfiles. The primary way to manage
     # plain files is through 'home.file'.
-    file =
-      {
-        # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-        # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-        # # symlink to the Nix store copy.
-        # ".screenrc".source = dotfiles/screenrc;
+    file = {
+      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+      # # symlink to the Nix store copy.
+      # ".screenrc".source = dotfiles/screenrc;
 
-        # # You can also set the file content immediately.
-        # ".gradle/gradle.properties".text = ''
-        #   org.gradle.console=verbose
-        #   org.gradle.daemon.idletimeout=3600000
-        # '';
-      }
-      // lib.mkIf (nodeConfig.isWorkstation && isLinux) {
-        ".local/share/jellyfinmediaplayer/scripts/mpris.so".source = lib.mkIf (
-          isLinux && nodeConfig.isWorkstation
-        ) "${pkgs.mpvScripts.mpris}/share/mpv/scripts/mpris.so";
-      };
+      # # You can also set the file content immediately.
+      # ".gradle/gradle.properties".text = ''
+      #   org.gradle.console=verbose
+      #   org.gradle.daemon.idletimeout=3600000
+      # '';
+    };
   };
 
   programs = {
     bash.enable = true;
     bat.enable = true;
     fd.enable = true;
-    password-store.enable = true;
-
-    man.generateCaches = true;
-
-    # Let Home Manager install and manage itself.
     home-manager.enable = true;
+    htop.enable = true;
+    man.generateCaches = true;
+    password-store.enable = true;
   };
 }

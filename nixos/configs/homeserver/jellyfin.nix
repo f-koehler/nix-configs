03ac7f4b6efb3bbde5849c9d810722a16ffa87ec
Fabian Koehler <fabian.koehler@proton.me>
2025-08-18 11:47:14 +0800

    sec(jellyfin): remove insecure intel-media-sdk package

--- nixos/configs/homeserver/jellyfin.nix
+++ nixos/configs/homeserver/jellyfin.nix
@@ -9,13 +9,13 @@ in
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
+      intel-compute-runtime
       intel-media-driver
       intel-vaapi-driver
-      vaapiVdpau
       libvdpau-va-gl
-      intel-compute-runtime
+      libvpl
+      vaapiVdpau
       vpl-gpu-rt
-      intel-media-sdk
     ];
   };
 

03b24a74aa341869b7de489059ccae8472e64c61
Fabian Koehler <fabian.koehler@proton.me>
2025-08-18 14:53:57 +0800

    feat(homeserver): disable tlp for now

--- nixos/configs/homeserver.nix
+++ nixos/configs/homeserver.nix
@@ -33,15 +33,15 @@
   };
 
   services = {
-    tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-        CPU_MIN_PERF_ON_AC = 5;
-        CPU_MAX_PERF_ON_AC = 40; # percent of max performance
-        CPU_BOOST_ON_AC = 0;
-      };
-    };
+    # tlp = {
+    #   enable = true;
+    #   settings = {
+    #     CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+    #     CPU_MIN_PERF_ON_AC = 5;
+    #     # CPU_MAX_PERF_ON_AC = 40; # percent of max performance
+    #     CPU_BOOST_ON_AC = 1;
+    #   };
+    # };
     zfs = {
       trim = {
         enable = true;

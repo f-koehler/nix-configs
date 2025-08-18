1b3facd62365985c14afbb62be07ae99c35709bf
Fabian Koehler <fabian.koehler@proton.me>
2025-08-18 16:49:08 +0800

    feat(mpd): setup mpris support for mpd

--- home/workstation/mpd.nix
+++ home/workstation/mpd.nix
@@ -1,11 +1,22 @@
-_: {
+{ config, ... }:
+{
   programs.ncmpcpp.enable = true;
-  services.mpd = {
-    enable = true;
-    musicDirectory = "/media/media1/soundtracks";
-    network = {
-      port = 6600;
-      startWhenNeeded = true;
+  services = {
+    mpd = {
+      enable = true;
+      musicDirectory = "/media/media1/soundtracks";
+      network = {
+        listenAddress = "127.0.0.1";
+        port = 6600;
+        startWhenNeeded = true;
+      };
+    };
+    mpdris2 = {
+      enable = true;
+      mpd = {
+        inherit (config.services.mpd.network) port;
+        host = config.services.mpd.network.listenAddress;
+      };
     };
   };
 }

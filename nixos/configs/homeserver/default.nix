fd33e5311334007647d79cf075c380c99be1bb4b
Fabian Koehler <fabian.koehler@proton.me>
2025-08-18 11:45:19 +0800

    refactor(homserver)!: comment out some currently unused services

--- nixos/configs/homeserver/default.nix
+++ nixos/configs/homeserver/default.nix
@@ -5,16 +5,16 @@ _: {
     ./anki.nix
     ./beszel-agent.nix
     ./beszel-hub.nix
-    ./forgejo.nix
-    ./gickup.nix
-    ./invidious.nix
+    # ./forgejo.nix
+    # ./gickup.nix
+    # ./invidious.nix
     ./jellyfin.nix
     ./nextcloud.nix
     ./nixos-containers.nix
     ./nginx.nix
     ./postgresql.nix
     ./samba.nix
-    ./searx.nix
+    # ./searx.nix
     ./tinymediamanager.nix
     ./unifi.nix
     ./zfs-snapshots.nix

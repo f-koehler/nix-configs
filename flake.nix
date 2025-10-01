c68ace756150ab7da55986752a33f10910fa97e4
Fabian Koehler <fabian.koehler@proton.me>
2025-10-01 11:04:18 +0800

    feat(pre-commit): integrate as checker, formatter, and in devshell

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   # Flake inputs
   inputs = {
+    git-hooks.url = "github:cachix/git-hooks.nix";
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
     systems.url = "github:nix-systems/default";
   };
@@ -12,25 +13,48 @@
     { self, ... }@inputs:
     let
       forEachSystem = inputs.nixpkgs.lib.genAttrs (import inputs.systems);
-      getNixpkgs = (
+      getNixpkgs =
         system:
         import inputs.nixpkgs {
           inherit system;
           config.allowUnfree = true;
-        }
-      );
+        };
     in
     {
+      checks = forEachSystem (system: {
+        pre-commit-check = inputs.git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            deadnix.enable = true;
+            flake-checker.enable = true;
+            nil.enable = true;
+            nixfmt-rfc-style.enable = true;
+            statix.enable = true;
+          };
+        };
+      });
+      formatter = forEachSystem (
+        system:
+        let
+          pkgs = getNixpkgs system;
+          inherit (self.checks.${system}) pre-commit-check;
+          script = ''
+            ${pre-commit-check.config.package}/bin/pre-commit run --all-files --config ${pre-commit-check.config.configFile}
+          '';
+        in
+        pkgs.writeShellScriptBin "pre-commit-run" script
+      );
       devShells = forEachSystem (system: {
         default =
           let
             pkgs = getNixpkgs system;
+            inherit (self.checks.${system}) pre-commit-check;
           in
           pkgs.mkShell {
-
+            buildInputs = pre-commit-check.enabledPackages;
             packages = with pkgs; [ ];
             env = { };
-            shellHook = "";
+            inherit (pre-commit-check) shellHook;
           };
       });
     };

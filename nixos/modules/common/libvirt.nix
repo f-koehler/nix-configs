7aba2efded880d648a668ff14f1f7e6e25b495f8
Fabian Koehler <fabian.koehler@proton.me>
2025-10-14 09:55:01 +0800

    fix(libvirt): remove deprecated config

--- nixos/modules/common/libvirt.nix
+++ nixos/modules/common/libvirt.nix
@@ -12,20 +12,6 @@ lib.mkIf nodeConfig.virtualisation {
       package = pkgs.qemu_kvm;
       runAsRoot = true;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          (pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          }).fd
-        ];
-      };
-      verbatimConfig = ''
-        nvram = [ "/run/libvirt/nix-ovmf/AAVMF_CODE.fd:/run/libvirt/nix-ovmf/AAVMF_VARS.fd", "/run/libvirt/nix-ovmf/OVMF_CODE.fd:/run/libvirt/nix-ovmf/OVMF_VARS.fd" ]
-        namespaces = []
-        remote_display_port_min = 5910
-      '';
     };
   };
   environment.systemPackages = with pkgs; [

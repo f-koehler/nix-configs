de0661679085badb8861f53662dc7297069286e3
Fabian Koehler <fabian.koehler@proton.me>
2025-10-28 09:43:25 +0800

    fix(ssh): update some deprecated ssh settings

--- home/common/ssh.nix
+++ home/common/ssh.nix
@@ -8,7 +8,20 @@ lib.mkIf nodeConfig.isWorkstation {
   services.ssh-agent.enable = isLinux;
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks = {
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
       "mbp2021" = {
         hostname = "mbp2021.corgi-dojo.ts.net";
         port = 22;

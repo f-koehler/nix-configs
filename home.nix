6004aa8dc9792aa8a03e522a4593d75a2c3ab341
Fabian Koehler <fabian@fkoehler.me>
2025-11-29 14:52:42 +0800

    feat(zsh): configure zsh

--- home.nix
+++ home.nix
@@ -121,7 +121,28 @@
     };
     zellij.enable = true;
     zoxide.enable = true;
-    zsh.enable = true;
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableVteIntegration = true;
+      autocd = true;
+      autosuggestion.enable = true;
+      history = {
+        append = true;
+        expireDuplicatesFirst = true;
+        extended = true;
+        findNoDups = true;
+        ignoreAllDups = true;
+        ignoreSpace = true;
+        save = 100000;
+        saveNoDups = true;
+        share = true;
+        size = 100000;
+      };
+      historySubstringSearch.enable = true;
+      setOptions = [ "NO_BEEP" ];
+      syntaxHighlighting.enable = true;
+    };
   };
   targets = {
     genericLinux = {

d47444c25a8f4794cdba69358be24c2824ec780a
Fabian Koehler <fabian@fkoehler.me>
2025-12-08 11:37:04 +0800

    feat(flatpak): extend flatpak selection

--- flatpak.nix
+++ flatpak.nix
@@ -4,5 +4,11 @@ _: {
     "org.inkscape.Inkscape"
     "org.libreoffice.LibreOffice"
     "org.videolan.VLC"
+    "org.libreoffice.LibreOffice"
+    "md.obsidian.Obsidian"
+    "org.videolan.VLC"
+    "org.zotero.Zotero"
+    "com.jgraph.drawio.desktop"
+    "com.super_productivity.SuperProductivity"
   ];
 }

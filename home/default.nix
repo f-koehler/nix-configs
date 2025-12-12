adbfc5b2a68215e159409095ea8c52f89a7a5a2c
Fabian Koehler <fabian@fkoehler.me>
2025-12-12 10:17:00 +0800

    feat(plasma): configure important global key bindings

--- home/default.nix
+++ home/default.nix
@@ -155,6 +155,53 @@
           Theme = "Papirus-Dark";
         };
       };
+      kglobalshortcutsrc = {
+        kwin = {
+          "Switch to Desktop 1" = "Meta+1,Ctrl+F1,Switch to Desktop 1";
+          "Switch to Desktop 2" = "Meta+2,Ctrl+F2,Switch to Desktop 2";
+          "Switch to Desktop 3" = "Meta+3,Ctrl+F3,Switch to Desktop 3";
+          "Switch to Desktop 4" = "Meta+4,Ctrl+F4,Switch to Desktop 4";
+          "Switch to Desktop 5" = "Meta+5,,Switch to Desktop 5";
+          "Switch to Desktop 6" = "Meta+6,,Switch to Desktop 6";
+          "Switch to Desktop 7" = "Meta+7,,Switch to Desktop 7";
+          "Switch to Desktop 8" = "Meta+8,,Switch to Desktop 8";
+          "Switch to Desktop 9" = "Meta+9,,Switch to Desktop 9";
+          "Switch to Desktop 10" = "Meta+0,,Switch to Desktop 10";
+          "Window to Desktop 1" = "Meta+!,,Window to Desktop 1";
+          "Window to Desktop 2" = "Meta+@,,Window to Desktop 2";
+          "Window to Desktop 3" = "Meta+#,,Window to Desktop 3";
+          "Window to Desktop 4" = "Meta+$,,Window to Desktop 4";
+          "Window to Desktop 5" = "Meta+%,,Window to Desktop 5";
+          "Window to Desktop 6" = "Meta+^,,Window to Desktop 6";
+          "Window to Desktop 7" = "Meta+&,,Window to Desktop 7";
+          "Window to Desktop 8" = "Meta+*,,Window to Desktop 8";
+          "Window to Desktop 9" = "Meta+(,,Window to Desktop 9";
+          "Show Desktop" = "none,Meta+D,Peek at Desktop";
+          "Window Fullscreen" = "Meta+F,,Make Window Fullscreen";
+        };
+
+        plasmashell = {
+          "activate task manager entry 1" = "none,Meta+1,Activate Task Manager Entry 1";
+          "activate task manager entry 10" = "none,,Activate Task Manager Entry 10";
+          "activate task manager entry 2" = "none,Meta+2,Activate Task Manager Entry 2";
+          "activate task manager entry 3" = "none,Meta+3,Activate Task Manager Entry 3";
+          "activate task manager entry 4" = "none,Meta+4,Activate Task Manager Entry 4";
+          "activate task manager entry 5" = "none,Meta+5,Activate Task Manager Entry 5";
+          "activate task manager entry 6" = "none,Meta+6,Activate Task Manager Entry 6";
+          "activate task manager entry 7" = "none,Meta+7,Activate Task Manager Entry 7";
+          "activate task manager entry 8" = "none,Meta+8,Activate Task Manager Entry 8";
+          "activate task manager entry 9" = "none,Meta+9,Activate Task Manager Entry 9";
+        };
+
+        services = {
+          "Alacritty.desktop" = {
+            New = "Meta+Return";
+          };
+          "org.kde.krunner.desktop" = {
+            _launch = "Meta+D\tSearch";
+          };
+        };
+      };
     };
   };
   services = {

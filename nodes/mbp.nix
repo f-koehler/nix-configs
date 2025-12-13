284302ea454a146a9ba860ed71606b46c889e4d4
Fabian Koehler <fabian@fkoehler.me>
2025-12-14 02:09:30 +0800

    feat(macos): configure aerospace, dock, and jankyborders

--- nodes/mbp.nix
+++ nodes/mbp.nix
@@ -4,9 +4,112 @@ _: {
     homeDirectory = "/Users/fkoehler";
   };
   targets.darwin = {
-    copyApps = {
+    copyApps.enable = true;
+    defaults = {
+      "com.apple.dock" = {
+        autohide = true;
+        orientation = "bottom";
+        size-immutable = true;
+        tilesize = 32;
+        minimize-to-application = true;
+        show-recents = false;
+        magnification = true;
+      };
+      "com.apple.menuextra.clock" = {
+        Show24Hour = true;
+        ShowAMPM = false;
+        ShowDate = 1;
+      };
+    };
+    currentHostDefaults = {
+      "com.apple.controlcenter".BatteryShowPercentage = true;
+    };
+  };
+  programs = {
+    aerospace = {
       enable = true;
-      enableChecks = true;
+      launchd.enable = true;
+      setttings = {
+        mode = {
+          config-version = 2;
+          persistent-workspaces = [ ];
+          main.binding = {
+            alt-enter = ''
+              exec-and-forget osascript -e '
+              tell application "Terminal"
+                  do script
+                  activate
+              end tell'
+            '';
+            alt-j = "focus --boundaries-action wrap-around-the-workspace left";
+            alt-k = "focus --boundaries-action wrap-around-the-workspace down";
+            alt-l = "focus --boundaries-action wrap-around-the-workspace up";
+            alt-semicolon = "focus --boundaries-action wrap-around-the-workspace right";
+
+            alt-shift-j = "move left";
+            alt-shift-k = "move down";
+            alt-shift-l = "move up";
+            alt-shift-semicolon = "move right";
+
+            alt-h = "split horizontal";
+            alt-v = "split vertical";
+
+            alt-f = "fullscreen";
+
+            alt-s = "layout v_accordion";
+            alt-w = "layout h_accordion";
+            alt-e = "layout tiles horizontal vertical";
+
+            alt-shift-space = "layout floating tiling";
+
+            alt-1 = "workspace 1";
+            alt-2 = "workspace 2";
+            alt-3 = "workspace 3";
+            alt-4 = "workspace 4";
+            alt-5 = "workspace 5";
+            alt-6 = "workspace 6";
+            alt-7 = "workspace 7";
+            alt-8 = "workspace 8";
+            alt-9 = "workspace 9";
+            alt-0 = "workspace 10";
+
+            alt-shift-1 = "move-node-to-workspace 1";
+            alt-shift-2 = "move-node-to-workspace 2";
+            alt-shift-3 = "move-node-to-workspace 3";
+            alt-shift-4 = "move-node-to-workspace 4";
+            alt-shift-5 = "move-node-to-workspace 5";
+            alt-shift-6 = "move-node-to-workspace 6";
+            alt-shift-7 = "move-node-to-workspace 7";
+            alt-shift-8 = "move-node-to-workspace 8";
+            alt-shift-9 = "move-node-to-workspace 9";
+            alt-shift-0 = "move-node-to-workspace 10";
+
+            alt-shift-c = "reload-config";
+
+            alt-r = "mode resize";
+          };
+          resize.binding = {
+            h = "resize width -50";
+            j = "resize height +50";
+            k = "resize height -50";
+            l = "resize width +50";
+            enter = "mode main";
+            esc = "mode main";
+          };
+        };
+      };
+    };
+  };
+  services = {
+    jankyborders = {
+      enable = true;
+      settings = {
+        width = 5.0;
+        hidpi = true;
+        style = "round";
+        active_color = "0xffcba6f7";
+        inactive_color = "0xff1e1e2e";
+      };
     };
   };
 }

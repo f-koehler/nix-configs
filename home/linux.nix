31f8e482b0f51a0d896432935bfc08db77358dcc
Fabian Koehler <fabian@fkoehler.me>
2026-05-06 23:18:42 +0800

    fix(gpu): update to home-manager change related to GPU setup

--- home/linux.nix
+++ home/linux.nix
@@ -1,9 +1,6 @@
 { lib, config, ... }:
 lib.mkIf config.targets.genericLinux.gpu.enable {
   home.activation.nonNixOSGPU = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    if [ -x /usr/sbin/restorecon ]; then
-      $DRY_RUN_CMD /usr/bin/sudo /usr/sbin/restorecon -v "${config.targets.genericLinux.gpu.setupPackage}/lib/systemd/system/non-nixos-gpu.service"
-    fi
     $DRY_RUN_CMD /usr/bin/sudo ${config.targets.genericLinux.gpu.setupPackage}/bin/non-nixos-gpu-setup
   '';
 }

98e5b9e47806ae699f17b98aad8e78d9e7d37c4b
Fabian Koehler <fabian@fkoehler.me>
2026-06-09 11:56:00 +0800

    fix(ssh): update and clean SSH config

--- home/ssh.nix
+++ home/ssh.nix
@@ -2,70 +2,38 @@ _: {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks = {
-      "*" = {
-        forwardAgent = false;
-        addKeysToAgent = "no";
-        compression = false;
-        serverAliveInterval = 0;
-        serverAliveCountMax = 3;
-        hashKnownHosts = false;
-        userKnownHostsFile = "~/.ssh/known_hosts";
-        controlMaster = "no";
-        controlPath = "~/.ssh/master-%r@%n:%p";
-        controlPersist = "no";
+    settings = {
+      "Host scbd1" = {
+        HostName = "10.1.128.104";
+        User = "scbd1";
       };
-      "scbd1" = {
-        hostname = "10.1.128.104";
-        user = "scbd1";
+      "Host scbd2" = {
+        HostName = "10.1.128.103";
+        User = "scbd2";
       };
-      "scbd2" = {
-        hostname = "10.1.128.103";
-        user = "scbd2";
+      "Host scbd3" = {
+        HostName = "10.1.128.107";
+        User = "scbd3";
       };
-      "scbd3" = {
-        hostname = "10.1.128.107";
-        user = "scbd3";
+      "Host scbd4" = {
+        HostName = "10.1.128.102";
+        User = "scbd4";
       };
-      "scbd4" = {
-        hostname = "10.1.128.102";
-        user = "scbd4";
+      "Host scbd5" = {
+        HostName = "10.1.245.52";
+        User = "scbd5";
       };
-      "scbd5" = {
-        hostname = "10.1.245.52";
-        user = "scbd5";
+      "Host scbd6" = {
+        HostName = "10.1.250.159";
+        User = "scbd6";
       };
-      "scbd6" = {
-        hostname = "10.1.250.159";
-        user = "scbd6";
+      "Host scbd7" = {
+        HostName = "10.1.178.70";
+        User = "scbd7";
       };
-      "scbd7" = {
-        hostname = "10.1.178.70";
-        user = "scbd7";
-      };
-      "dnsmasq" = {
-        hostname = "10.1.3.1";
-        user = "root";
-      };
-      "pve-beowulf-0" = {
-        hostname = "10.1.206.113";
-        user = "root";
-      };
-      "pve-beowulf-1" = {
-        hostname = "10.1.142.246";
-        user = "root";
-      };
-      "fk-gha-runner-x64-005" = {
-        hostname = "10.1.218.102";
-        user = "fkoehler";
-      };
-      "fk-gha-runner-x64-006" = {
-        hostname = "10.1.255.49";
-        user = "fkoehler";
-      };
-      "fk-gha-runner-x64-007" = {
-        hostname = "10.1.242.35";
-        user = "fkoehler";
+      "Host dnsmasq" = {
+        HostName = "10.1.3.1";
+        User = "root";
       };
     };
   };

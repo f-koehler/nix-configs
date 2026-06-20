0bb153b456a884a94383bd962b8bf2cfb0d8cac1
Fabian Koehler <fabian@fkoehler.me>
2026-06-20 11:31:46 +0800

    feat(firefox)!: remove bitwarden extension

--- home/firefox.nix
+++ home/firefox.nix
@@ -117,14 +117,6 @@
           private_browsing = true;
         };
 
-        # bitwarden
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          default_area = "menupanel";
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/{446900e4-71c2-419f-a6a7-df9c091e268b}/latest.xpi";
-          installation_mode = "force_installed";
-          private_browsing = false;
-        };
-
         # Zotero
         "zotero@chnm.gmu.edu" = {
           default_area = "menuarea";

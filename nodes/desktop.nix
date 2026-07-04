e9974e4594e6af265df4098d86a1729a7dcac4a8
Fabian Koehler <fabian@fkoehler.me>
2026-07-04 20:52:49 +0800

    fix(desktop): update nvidia driver version

--- nodes/desktop.nix
+++ nodes/desktop.nix
@@ -10,8 +10,8 @@ _: {
         enable = true;
         nvidia = {
           enable = true;
-          sha256 = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
-          version = "595.71.05";
+          sha256 = "sha256-MDSgVLtM33dS/43CclZMsQVROAS/9TU4lFkBsWyndGM=";
+          version = "610.43.02";
         };
       };
     };

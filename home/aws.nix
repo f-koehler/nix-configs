f4eac6afac7b262fd00dc0ee5a9ec8bfd02df3b6
Fabian Koehler <fabian@fkoehler.me>
2026-07-24 17:45:15 +0800

    feat(aws): add script to get s3 usage (per bucket and total)

--- home/aws.nix
+++ home/aws.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   sops = {
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
@@ -34,4 +34,41 @@
       source = config.lib.file.mkOutOfStoreSymlink "${config.sops.templates."aws-credentials".path}";
     };
   };
+
+  home.packages = [
+    (pkgs.writeShellApplication {
+      name = "s3-bucket-sizes";
+      runtimeInputs = with pkgs; [
+        awscli2
+        coreutils
+        gawk
+        util-linux
+      ];
+      text = ''
+        declare -a rows=()
+        total_bytes=0
+
+        for bucket in $(aws s3api list-buckets --query "Buckets[].Name" --output text); do
+            summary=$(aws s3 ls "s3://$bucket" --recursive --summarize 2>/dev/null | tail -n 2)
+            objects=$(awk -F': ' '/Total Objects/{print $2}' <<<"$summary")
+            bytes=$(awk -F': ' '/Total Size/{print $2}' <<<"$summary")
+            objects=''${objects:-0}
+            bytes=''${bytes:-0}
+
+            total_bytes=$((total_bytes + bytes))
+            human=$(numfmt --to=iec-i --suffix=B --format="%.2f" "$bytes")
+
+            rows+=("$bucket|$objects|$human")
+        done
+
+        {
+            echo "Bucket|Objects|Size"
+            printf '%s\n' "''${rows[@]}"
+        } | column -t -s '|'
+
+        echo
+        echo "Total: $(numfmt --to=iec-i --suffix=B --format="%.2f" "$total_bytes") across ''${#rows[@]} buckets"
+      '';
+    })
+  ];
 }

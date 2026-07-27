36576aa3964940a75f464e2c667b90c276fab552
Fabian Koehler <fabian@fkoehler.me>
2026-07-27 21:43:55 +0800

    refactor(nodes): remove Nvidia workaround on desktop

--- nodes/desktop.nix
+++ nodes/desktop.nix
@@ -29,25 +29,4 @@ _: {
       };
     };
   };
-  # home-manager's GPU module calls .override { kernel = null; } but nixpkgs removed `kernel` from nvidia generic.nix in ebc08544
-  nixpkgs.overlays = [
-    (_: prev: {
-      linuxPackages = prev.linuxPackages.extend (
-        _: lprev: {
-          nvidiaPackages = lprev.nvidiaPackages // {
-            mkDriver =
-              driverArgs:
-              let
-                base = lprev.nvidiaPackages.mkDriver driverArgs;
-                origOverride = base.override;
-              in
-              base
-              // {
-                override = overrideArgs: origOverride (builtins.removeAttrs overrideArgs [ "kernel" ]);
-              };
-          };
-        }
-      );
-    })
-  ];
 }

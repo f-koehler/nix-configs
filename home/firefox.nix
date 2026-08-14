ed422ab255c758daf8d9db8b2d1a2d3bd26ae36e
Fabian Koehler <fabian@fkoehler.me>
2026-08-14 12:26:33 +0800

    feat(firefox): add some work related bookmarks

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,4 +1,10 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  hasTag,
+  ...
+}:
 {
   # The nixpkgs Firefox wrapper sets MOZ_LEGACY_PROFILES=1, forcing Firefox to
   # read profiles from ~/.mozilla/firefox/ instead of the XDG path that
@@ -83,6 +89,23 @@
       bookmarks = {
         force = true;
         settings = [
+          (lib.mkIf (hasTag "speqtral") {
+            name = "SpeQtral";
+            bookmarks = [
+              {
+                name = "Redmine (All Projects)";
+                url = "http://10.1.5.5:3000/agile/board?set_filter=1&f%5B%5D=assigned_to_id&op%5Bassigned_to_id%5D=%3D&v%5Bassigned_to_id%5D%5B%5D=me&f%5B%5D=status_id&op%5Bstatus_id%5D=%3D&f_status%5B%5D=1&f_status%5B%5D=7&f_status%5B%5D=2&f_status%5B%5D=4&f_status%5B%5D=8&f_status%5B%5D=11&f_status%5B%5D=10&f_status%5B%5D=6&f_status%5B%5D=9&f_status%5B%5D=12&f_status%5B%5D=16&f_status%5B%5D=17&c%5B%5D=tracker&c%5B%5D=estimated_hours&c%5B%5D=spent_hours&c%5B%5D=done_ratio&c%5B%5D=parent&c%5B%5D=assigned_to&c%5B%5D=cf_4&c%5B%5D=cf_3";
+              }
+              {
+                name = "pve-qnex-apps";
+                url = "pve-qnex-apps.speqtranet.com:8006";
+              }
+              {
+                name = "GitHub Actions Runners";
+                url = "https://github.com/organizations/SpeQtral/settings/actions/runners";
+              }
+            ];
+          })
           {
             name = "Nix";
             bookmarks = [
@@ -101,8 +124,17 @@
             ];
           }
           {
-            name = "GitHub";
-            url = "https://github.com";
+            name = "C++";
+            bookmarks = [
+              {
+                name = "cppreference";
+                url = "https://en.cppreference.com/w/";
+              }
+              {
+                name = "C++ Standard Draft";
+                url = "https://eel.is/c++draft/";
+              }
+            ];
           }
         ];
       };

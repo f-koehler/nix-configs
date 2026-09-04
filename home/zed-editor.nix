4b7e073a762180694b12a0b78bdc3c42a8786497
Fabian Koehler <fabian@fkoehler.me>
2026-09-05 00:16:58 +0800

    fix(zed): fix zed start by using its FHS variant

--- home/zed-editor.nix
+++ home/zed-editor.nix
@@ -3,7 +3,7 @@
   home.packages = [ pkgs.texlab ];
   programs.zed-editor = {
     enable = true;
-    package = pkgs.zed-editor;
+    package = pkgs.zed-editor-fhs;
     installRemoteServer = true;
     extensions = [
       "docker-compose"

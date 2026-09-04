7bafed1f9bebbf4c4c6a2b8aecc6da197ba4b35d
Fabian Koehler <fabian@fkoehler.me>
2026-09-05 00:17:37 +0800

    fix(nvidia): re-enable shim

--- nodes/desktop.nix
+++ nodes/desktop.nix
@@ -19,7 +19,14 @@ _: {
   targets = {
     genericLinux = {
       enable = true;
-      gpu.enable = false;
+      gpu = {
+        enable = true;
+        nvidia = {
+          enable = true;
+          version = "610.57.04";
+          sha256 = "sha256-suk1xmuDuwDAyFe8jg7g/VLekoa0DJzB7sKafOfrEW0=";
+        };
+      };
     };
   };
 }

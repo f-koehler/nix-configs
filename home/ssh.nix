bc64d47426cb64bd48efdb894d8131064ee9d656
Fabian Koehler <fabian@fkoehler.me>
2026-09-06 14:13:48 +0000

    refactor(ssh): add support for encrypted SSH hosts

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,55 +1,23 @@
-_: {
+{
+  config,
+  lib,
+  hasTag,
+  ...
+}:
+{
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
+    includes = lib.optional (hasTag "speqtral") config.sops.secrets."ssh-internal_hosts".path;
     settings = {
-      "Host scbd1" = {
-        HostName = "10.1.128.104";
-        User = "scbd1";
-      };
-      "Host scbd2" = {
-        HostName = "10.1.128.103";
-        User = "scbd2";
-      };
-      "Host scbd3" = {
-        HostName = "192.168.2.177";
-        User = "scbd3";
-      };
-      "Host scbd4" = {
-        HostName = "192.168.2.216";
-        User = "scbd4";
-      };
-      "Host scbd5" = {
-        HostName = "10.1.245.52";
-        User = "scbd5";
-      };
-      "Host scbd6" = {
-        HostName = "10.1.250.159";
-        User = "scbd6";
-      };
-      "Host scbd7" = {
-        HostName = "10.1.178.70";
-        User = "scbd7";
-      };
-      "Host dnsmasq" = {
-        HostName = "10.1.3.1";
-        User = "root";
-      };
-      "Host ogs3" = {
-        User = "ogs3";
-        HostName = "10.1.157.92";
-      };
-      "Host ogs2" = {
-        User = "ogs2";
-        HostName = "10.1.217.86";
-      };
-      "Host fk-temp27.speqtranet.com" = {
-        ForwardAgent = "yes";
-      };
       "Host homeserver" = {
         User = "fkoehler";
         HostName = "homeserver.corgi-dojo.ts.net";
       };
     };
+  };
+
+  sops.secrets."ssh/internal_hosts" = lib.mkIf (hasTag "speqtral") {
+    sopsFile = ./../secrets/ssh-hosts.yaml;
   };
 }

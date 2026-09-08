4456fddf0351a9ecb1951ce71b844854b4c75ba9
Fabian Koehler <fabian@fkoehler.me>
2026-09-09 01:08:12 +0800

    feat(neovim): make sure npm is installed

--- home/neovim.nix
+++ home/neovim.nix
@@ -5,6 +5,7 @@
   ];
   programs.ripgrep.enable = true;
   programs.fd.enable = true;
+  programs.npm.enable = true;
   xdg.configFile."nvim".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Code/nix-configs/home/neovim-config";
 }
